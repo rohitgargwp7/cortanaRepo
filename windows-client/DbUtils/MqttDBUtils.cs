c36eb63b2288e32a5852da00cddb40dc0997a5f1
unknown <madhur@madhur-PC.(none)>
2012-06-12 13:32:19 +0530

    Re-arranged functions for blocklist, mqttpersistence & thumbails.

---
+++ windows-client/DbUtils/MqttDBUtils.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using windows_client.Model;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.Linq;
+
+namespace windows_client.DbUtils
+{
+    public class MqttDBUtils
+    {
+        #region MqttPersistence
+        public static List<HikePacket> getAllSentMessages()
+        {
+            Func<HikeDataContext, IQueryable<HikePacket>> q =
+            CompiledQuery.Compile<HikeDataContext, IQueryable<HikePacket>>
+            ((HikeDataContext hdc) =>
+                from o in hdc.mqttMessages
+                select o);
+            return q(App.HikeDataContext).Count<HikePacket>() == 0 ? null :
+                q(App.HikeDataContext).ToList<HikePacket>();
+        }
+
+        public static void addSentMessage(HikePacket packet)
+        {
+            HikePacket mqttMessage = new HikePacket(packet.MessageId, packet.Message, packet.Timestamp);
+            App.HikeDataContext.mqttMessages.InsertOnSubmit(mqttMessage);
+            App.HikeDataContext.SubmitChanges();
+            //TODO update observable list
+        }
+
+        public void removeSentMessage(long msgId)
+        {
+            Func<HikeDataContext, long, IQueryable<HikePacket>> q =
+            CompiledQuery.Compile<HikeDataContext, long, IQueryable<HikePacket>>
+            ((HikeDataContext hdc, long id) =>
+                from o in hdc.mqttMessages
+                where o.MessageId == id
+                select o);
+            App.HikeDataContext.mqttMessages.DeleteAllOnSubmit<HikePacket>(q(App.HikeDataContext, msgId));
+            App.HikeDataContext.SubmitChanges();
+        }
+        #endregion
+
+
+    }
+}
---
+++ windows-client/Model/MiscDatabase.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Data.Linq.Mapping;
+using System.Data.Linq;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+
+
+namespace windows_client.Model
+{
+
+    [Table(Name = "blocked")]
+    public class Blocked : INotifyPropertyChanged, INotifyPropertyChanging
+    {
+
+        private string _msisdn;
+
+        [Column(IsPrimaryKey = true, CanBeNull = false)]
+        public string Msisdn
+        {
+            get
+            {
+                return _msisdn;
+            }
+            set
+            {
+                if (_msisdn != value)
+                {
+                    NotifyPropertyChanging("Msisdn");
+                    _msisdn = value;
+                    NotifyPropertyChanged("Msisdn");
+                }
+            }
+        }
+
+        public Blocked(string msisdn)
+        {
+            this.Msisdn = msisdn;
+        }
+
+        public Blocked()
+        { 
+        }
+
+        #region INotifyPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+                // Used to notify that a property changed
+        private void NotifyPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        #endregion
+
+        #region INotifyPropertyChanging Members
+
+        public event PropertyChangingEventHandler PropertyChanging;
+
+        // Used to notify that a property is about to change
+        private void NotifyPropertyChanging(string propertyName)
+        {
+            if (PropertyChanging != null)
+            {
+                PropertyChanging(this, new PropertyChangingEventArgs(propertyName));
+            }
+        }
+
+        #endregion
+    }
+
+    [Table(Name = "thumbnails")]
+
+    public class Thumbnails : INotifyPropertyChanged, INotifyPropertyChanging
+    {
+        #region members
+        private string _msisdn;
+        private byte[] _avatar;
+        #endregion
+
+        #region properties
+        [Column(IsPrimaryKey = true, CanBeNull = false)]
+        public string Msisdn
+        {
+            get
+            {
+                return _msisdn;
+            }
+            set
+            {
+                if (_msisdn != value)
+                {
+                    NotifyPropertyChanging("Msisdn");
+                    _msisdn = value;
+                    NotifyPropertyChanged("Msisdn");
+                }
+            }
+        }
+
+
+        [Column(CanBeNull = false)]
+        public byte[] Avatar
+        {
+            get
+            {
+                return _avatar;
+            }
+            set
+            {
+                if (_avatar != value)
+                {
+                    NotifyPropertyChanging("Avatar");
+                    _avatar = value;
+                    NotifyPropertyChanged("Avatar");
+                }
+            }
+        }
+        #endregion
+
+        #region contructors
+        public Thumbnails()
+        { 
+        }
+
+        public Thumbnails(string msisdn, byte[] image)
+        {
+            this.Msisdn = msisdn;
+            this.Avatar = image;
+        }
+
+        #endregion
+        #region INotifyPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        // Used to notify that a property changed
+        private void NotifyPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        #endregion
+
+        #region INotifyPropertyChanging Members
+
+        public event PropertyChangingEventHandler PropertyChanging;
+
+        // Used to notify that a property is about to change
+        private void NotifyPropertyChanging(string propertyName)
+        {
+            if (PropertyChanging != null)
+            {
+                PropertyChanging(this, new PropertyChangingEventArgs(propertyName));
+            }
+        }
+
+        #endregion
+
+    }
+}
