97eb9cfdee720111ab8014a364bc13a3047e2f6a
Madhur Garg <mg@bsb.in>
2012-06-19 14:36:06 +0530

    Added converter for UI

---
+++ windows-client/converters/ChatBubbleDirection.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Windows.Data;
+
+namespace windows_client.converters
+{
+    public class ChatBubbleDirection : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            bool isSent = (bool)value;
+            if (isSent)
+            {
+                return "LowerRight";
+            }
+            return "LowerLeft";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    }
+
---
+++ windows-client/converters/ImageConverter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Windows.Data;
+using System.IO;
+using System.Windows.Media.Imaging;
+using System.Resources;
+using System.Windows.Resources;
+using System.Reflection;
+
+namespace windows_client.converters
+{
+    public class ImageConverter : IValueConverter
+    {
+        private static BitmapImage defaultBitmapImage;
+        public ImageConverter()
+        {
+            if (defaultBitmapImage == null)
+            {
+                Uri uri = new Uri("/View/images/ic_avatar0.png", UriKind.Relative);
+                defaultBitmapImage = new BitmapImage(uri);
+            }
+        }
+
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value == null)
+                return defaultBitmapImage;
+            MemoryStream memStream = new MemoryStream((byte[])value);
+            memStream.Seek(0, SeekOrigin.Begin);
+            BitmapImage empImage = new BitmapImage();
+            empImage.SetSource(memStream);
+            return empImage;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
