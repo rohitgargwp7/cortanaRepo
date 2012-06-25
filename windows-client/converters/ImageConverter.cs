ea18705976e52b15838b99d27c58338f7c2882a0
Madhur Garg <mg@bsb.in>
2012-06-25 18:02:52 +0530

    Added caching of images

--- windows-client/converters/ImageConverter.cs
+++ windows-client/converters/ImageConverter.cs
@@ -14,30 +14,73 @@ using System.Windows.Media.Imaging;
 using System.Resources;
 using System.Windows.Resources;
 using System.Reflection;
+using System.Collections.Generic;
+using windows_client.DbUtils;
+using windows_client.Model;
 
 namespace windows_client.converters
 {
     public class ImageConverter : IValueConverter
     {
         private static BitmapImage defaultBitmapImage;
+        private static Dictionary<string, BitmapImage> imageCache;
+        private static List<string> numbersWithDefaultImage; 
+
         public ImageConverter()
         {
-            if (defaultBitmapImage == null)
+            imageCache = new Dictionary<string, BitmapImage>();
+            Uri uri = new Uri("/View/images/ic_avatar0.png", UriKind.Relative);
+            defaultBitmapImage = new BitmapImage(uri);
+            numbersWithDefaultImage = new List<string>();
+        }
+
+        public static void updateImageInCache(string msisdn, byte[] imageBytes)
+        {
+            if (!numbersWithDefaultImage.Contains(msisdn) && !imageCache.ContainsKey(msisdn))
+                return;
+
+            if (numbersWithDefaultImage.Contains(msisdn))
             {
-                Uri uri = new Uri("/View/images/ic_avatar0.png", UriKind.Relative);
-                defaultBitmapImage = new BitmapImage(uri);
+                numbersWithDefaultImage.Remove(msisdn);
             }
+            else if (imageCache.ContainsKey(msisdn))
+            {
+                imageCache.Remove(msisdn);
+            }
+            MemoryStream memStream = new MemoryStream(imageBytes);
+            memStream.Seek(0, SeekOrigin.Begin);
+            BitmapImage empImage = new BitmapImage();
+            empImage.SetSource(memStream);
+            imageCache.Add(msisdn, empImage);
         }
 
 
         public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
             if (value == null)
+                return null;
+
+            string msisdn = (string)value;
+            if (imageCache.ContainsKey(msisdn))
+            {
+                BitmapImage cachedImage;
+                imageCache.TryGetValue(msisdn, out cachedImage);
+                return cachedImage;
+            }
+            if (numbersWithDefaultImage.Contains(msisdn))
                 return defaultBitmapImage;
-            MemoryStream memStream = new MemoryStream((byte[])value);
+
+            Thumbnails thumbnail = MiscDBUtil.getThumbNailForMSisdn(msisdn);
+            if (thumbnail == null)
+            {
+                numbersWithDefaultImage.Add(msisdn);
+                return defaultBitmapImage;
+            }
+            MemoryStream memStream = new MemoryStream((byte[])thumbnail.Avatar);
             memStream.Seek(0, SeekOrigin.Begin);
             BitmapImage empImage = new BitmapImage();
             empImage.SetSource(memStream);
+            imageCache[msisdn] = empImage;
             return empImage;
         }
 
