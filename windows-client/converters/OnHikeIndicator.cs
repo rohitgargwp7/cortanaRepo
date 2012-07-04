71e98658e23070b0e5bc2c0b41ee1a822d241bba
madhurgarg <mg@bsb.in>
2012-07-04 13:09:04 +0530

    Added functionality to show hike status while selecting a user for new message. Project file as build action of added images changed

--- windows-client/converters/OnHikeIndicator.cs
+++ windows-client/converters/OnHikeIndicator.cs
@@ -9,18 +9,44 @@ using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using System.Windows.Media.Imaging;
+using System.Windows.Data;
 
 namespace windows_client.converters
 {
-    public class OnHikeIndicator
+    public class OnHikeIndicator : IValueConverter
     {
         private static BitmapImage onHikeImage;
         private static BitmapImage notOnHikeImage;
         
         public OnHikeIndicator()
-        { 
-        
+        {
+            if (onHikeImage == null)
+            {
+                Uri uri = new Uri("/View/images/ic_hike_user.png", UriKind.Relative);
+                onHikeImage = new BitmapImage(uri);
+            }
+            if (notOnHikeImage == null)
+            {
+                Uri uri = new Uri("/View/images/ic_sms_user.png", UriKind.Relative);
+                notOnHikeImage = new BitmapImage(uri);
+            }
         }
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            bool onHike = (bool)value;
+            if (onHike)
+            {
+                return onHikeImage;
+            }
+            return notOnHikeImage;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
 
     }
 }
