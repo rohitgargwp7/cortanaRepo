fcc6c26152387d6804104b172b88bcebf7297b4e
Gautam Kanwar <gk@bsb.in>
2012-07-12 17:21:00 +0530

    Removed unused broadcastevents

--- windows-client/DbUtils/DbConversationListener.cs
+++ windows-client/DbUtils/DbConversationListener.cs
@@ -1,7 +1,7 @@
-﻿using windows_client.Model;
+﻿using System.IO;
 using Newtonsoft.Json.Linq;
 using windows_client.converters;
-using System.IO;
+using windows_client.Model;
 
 namespace windows_client.DbUtils
 {
@@ -21,45 +21,28 @@ namespace windows_client.DbUtils
         private void registerListeners()
         {
             mPubSub.addListener(HikePubSub.MESSAGE_SENT, this);
-            mPubSub.addListener(HikePubSub.MESSAGE_RECEIVED_FROM_SENDER, this);
             mPubSub.addListener(HikePubSub.MESSAGE_RECEIVED_READ, this);
-            mPubSub.addListener(HikePubSub.SERVER_RECEIVED_MSG, this);
-            mPubSub.addListener(HikePubSub.MESSAGE_DELIVERED, this);
-            mPubSub.addListener(HikePubSub.MESSAGE_DELIVERED_READ, this);
             mPubSub.addListener(HikePubSub.MESSAGE_DELETED, this);
             mPubSub.addListener(HikePubSub.MESSAGE_FAILED, this);
             mPubSub.addListener(HikePubSub.BLOCK_USER, this);
             mPubSub.addListener(HikePubSub.UNBLOCK_USER, this);
-            mPubSub.addListener(HikePubSub.SMS_CREDIT_CHANGED, this);
-            mPubSub.addListener(HikePubSub.USER_JOINED, this);
-            mPubSub.addListener(HikePubSub.USER_LEFT, this);
-            mPubSub.addListener(HikePubSub.ICON_CHANGED, this);
             mPubSub.addListener(HikePubSub.ADD_OR_UPDATE_PROFILE, this);
         }
 
         private void removeListeners()
         {
            mPubSub.removeListener(HikePubSub.MESSAGE_SENT, this);
-           mPubSub.removeListener(HikePubSub.MESSAGE_RECEIVED_FROM_SENDER, this);
            mPubSub.removeListener(HikePubSub.MESSAGE_RECEIVED_READ, this);
-           mPubSub.removeListener(HikePubSub.SERVER_RECEIVED_MSG, this);
-           mPubSub.removeListener(HikePubSub.MESSAGE_DELIVERED, this);
-           mPubSub.removeListener(HikePubSub.MESSAGE_DELIVERED_READ, this);
            mPubSub.removeListener(HikePubSub.MESSAGE_DELETED, this);
            mPubSub.removeListener(HikePubSub.MESSAGE_FAILED, this);
            mPubSub.removeListener(HikePubSub.BLOCK_USER, this);
            mPubSub.removeListener(HikePubSub.UNBLOCK_USER, this);
-           mPubSub.removeListener(HikePubSub.SMS_CREDIT_CHANGED, this);
-           mPubSub.removeListener(HikePubSub.USER_JOINED, this);
-           mPubSub.removeListener(HikePubSub.USER_LEFT, this);
-           mPubSub.removeListener(HikePubSub.ICON_CHANGED, this);
            mPubSub.removeListener(HikePubSub.ADD_OR_UPDATE_PROFILE, this);
         }
 
         public void onEventReceived(string type, object obj)
         {
-           /* ThreadPool.QueueUserWorkItem((object o) =>
-                {*/
+           
                     #region MESSAGE_SENT
                     if (HikePubSub.MESSAGE_SENT == type)
                     {
@@ -72,16 +55,6 @@ namespace windows_client.DbUtils
                         mPubSub.publish(HikePubSub.MQTT_PUBLISH, convMessage.serialize());
                     }
                     #endregion
-                    #region MESSAGE_RECEIVED_FROM_SENDER
-                    else if (HikePubSub.MESSAGE_RECEIVED_FROM_SENDER == type)  // represents event when a client receive msg from other client through server.
-                    {
-                        ConvMessage convMessage = (ConvMessage)obj;
-                        convMessage.MessageStatus = ConvMessage.State.RECEIVED_UNREAD;
-                        MessagesTableUtils.addChatMessage(convMessage);
-                        logger.Info("DBCONVERSATION LISTENER", "Receiver received Message : " + convMessage.Message + " ; Receiver Msg ID : " + convMessage.MessageId + "	; Mapped msgID : " + convMessage.MappedMessageId);
-                        mPubSub.publish(HikePubSub.MESSAGE_RECEIVED, convMessage);
-                    }
-                    #endregion
                     #region MESSAGE_RECEIVED_READ
                     else if (HikePubSub.MESSAGE_RECEIVED_READ == type)  // represents event when a msg is read by this user
                     {
@@ -89,32 +62,22 @@ namespace windows_client.DbUtils
                         updateDbBatch(ids, (int)ConvMessage.State.RECEIVED_READ);
                     }
                     #endregion
-                    #region SERVER_RECEIVED_MSG
-                    else if (HikePubSub.SERVER_RECEIVED_MSG == type)  // server got msg from client 1 and sent back received msg receipt
-                    {
-                        logger.Info("DBCONVERSATION LISTENER", "(Sender) Message sent confirmed for msgID -> " + (long)obj);
-                        updateDB(obj, (int)ConvMessage.State.SENT_CONFIRMED);
-                    }
-                    #endregion
-                    #region MESSAGE_DELIVERED
-                    else if (HikePubSub.MESSAGE_DELIVERED == type)  // server got msg from client 1 and sent back received msg receipt
-                    {
-                        logger.Info("DBCONVERSATION LISTENER", "Msg delivered to receiver for msgID -> " + (long)obj);
-                        updateDB(obj, (int)ConvMessage.State.SENT_DELIVERED);
-                    }
-                    #endregion
-                    #region MESSAGE_DELIVERED_READ
-                    else if (HikePubSub.MESSAGE_DELIVERED_READ == type)  // server got msg from client 1 and sent back received msg receipt
-                    {
-                        long[] ids = (long[])obj;
-                        logger.Info("DBCONVERSATION LISTENER", "Message delivered read for ids " + ids);
-                        updateDbBatch(ids, (int)ConvMessage.State.SENT_DELIVERED_READ);
-                    }
-                    #endregion
                     #region MESSAGE_DELETED
                     else if (HikePubSub.MESSAGE_DELETED == type)
                     {
-                        long msgId = (long)obj;
+                        object[] o = (object[])obj;
+
+                        long msgId = (long)o[0];
+                        MessagesTableUtils.deleteMessage(msgId); // delete msg with given msgId from messages table
+
+                        bool delConv = (bool)o[2];
+                        if (delConv)
+                        {
+                            string msisdn = (string)o[1];
+                            // delete the conversation from DB.
+                            ConversationTableUtils.deleteConversation(msisdn);
+                        }
+
                         MessagesTableUtils.deleteMessage(msgId);
                         // TODO :: persistence.removeMessage(msgId);
                     }
@@ -143,26 +106,7 @@ namespace windows_client.DbUtils
                         mPubSub.publish(HikePubSub.MQTT_PUBLISH, unblockObj);
                     }
                     #endregion
-                    #region USER_JOINED/LEFT
-                    else if (HikePubSub.USER_JOINED == type || HikePubSub.USER_LEFT == type)
-                    {
-                        string msisdn = (string)obj;
-                        bool joined = HikePubSub.USER_JOINED == type;
-                        UsersTableUtils.updateOnHikeStatus(msisdn, joined);
-                        ConversationTableUtils.updateOnHikeStatus(msisdn, joined);
-                    }
-                    #endregion
-                    #region ICON_CHANGED
-                    else if (HikePubSub.ICON_CHANGED == type)
-                    {
-                        object[] data = (object[])obj;
-                        string msisdn = (string)data[0];
-                        byte[] imageBytes = (byte[])data[1];
-                        MiscDBUtil.addOrUpdateIcon(msisdn, imageBytes);
-                        ImageConverter.updateImageInCache(msisdn, imageBytes);
-                        mPubSub.publish(HikePubSub.UPDATE_UI, msisdn);
-                    }
-                    #endregion
+                    
                     #region ADD_OR_UPDATE_PROFILE
                     else if (HikePubSub.ADD_OR_UPDATE_PROFILE == type)
                     {
@@ -174,17 +118,7 @@ namespace windows_client.DbUtils
                         MiscDBUtil.addOrUpdateProfileIcon(msisdn + "::large", msLargeImage.ToArray());
                     }
                     #endregion
-              /*  });*/
 
-                    #region SMS_CREDIT_CHANGED
-                    if (HikePubSub.SMS_CREDIT_CHANGED == type)  // server got msg from client 1 and sent back received msg receipt
-                    {
-                        /* Save credits to isolated storage */
-                        int credits = (int)obj;
-                        App.appSettings[App.SMS_SETTING] = credits;
-                        App.appSettings.Save();
-                    }
-                    #endregion
         }
 
         private JObject blockUnblockSerialize(string type, string msisdn)
@@ -205,5 +139,6 @@ namespace windows_client.DbUtils
         {
             MessagesTableUtils.updateAllMsgStatus(ids, status);
         }
+
     }
 }
