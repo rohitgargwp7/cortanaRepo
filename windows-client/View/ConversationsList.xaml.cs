0b4da394027264ab78381de889ca4f81f2b98891
madhurgarg <mg@bsb.in>
2012-08-10 17:50:46 +0530

    Removing app bar button for group chat from this branch

--- windows-client/View/ConversationsList.xaml.cs
+++ windows-client/View/ConversationsList.xaml.cs
@@ -248,13 +248,6 @@ namespace windows_client.View
             composeIconButton.IsEnabled = true;
             appBar.Buttons.Add(composeIconButton);
 
-            ApplicationBarIconButton groupChatIconButton = new ApplicationBarIconButton();
-            groupChatIconButton.IconUri = new Uri("/View/images/icon_group_chat.png", UriKind.Relative);
-            groupChatIconButton.Text = "Group Chat";
-            groupChatIconButton.Click += new EventHandler(groupChatBtn_Click);
-            groupChatIconButton.IsEnabled = true;
-            appBar.Buttons.Add(groupChatIconButton);
-
             /* Add Menu Items*/
             ApplicationBarMenuItem inviteUsersMenu = new ApplicationBarMenuItem();
             inviteUsersMenu.Text = INVITE_USERS;
