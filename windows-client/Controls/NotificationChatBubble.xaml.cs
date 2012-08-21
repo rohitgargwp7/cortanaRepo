5268e7960b74d914dcede23990c6772b30411ef2
madhurgarg <mg@bsb.in>
2012-08-21 19:58:56 +0530

    Design of notification messages

--- windows-client/Controls/NotificationChatBubble.xaml.cs
+++ windows-client/Controls/NotificationChatBubble.xaml.cs
@@ -7,13 +7,29 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
+using windows_client.Model;
+using windows_client.utils;
 
 namespace windows_client.Controls
 {
     public partial class NotificationChatBubble : MyChatBubble {
-        public NotificationChatBubble() {
+        public NotificationChatBubble(string message, bool onHike) {
             // Required to initialize variables
             InitializeComponent();
+            if (!String.IsNullOrEmpty(message))
+            {
+                this.UserName.Text = message;
+            }
+            this.OnHikeImage.Source = UI_Utils.Instance.OnHikeImage;
+            if (onHike)
+            {
+                //this.HikeBubble.Source = UI_Utils.Instance.MessageReadBitmapImage;
+            }
+            else
+            {
+                //this.HikeBubble.Source = UI_Utils.Instance.DefaultAvatarBitmapImage;
+
+            }
         }
     }
 }
