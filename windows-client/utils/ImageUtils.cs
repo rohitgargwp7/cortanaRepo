512e410ab72285034eddcc813e5751d23ffb4fe9
madhurgarg <mg@bsb.in>
2012-08-21 20:01:16 +0530

    Moved images and colors to this class

--- windows-client/utils/ImageUtils.cs
+++ windows-client/utils/ImageUtils.cs
@@ -4,11 +4,18 @@ using System.Collections.Generic;
 using windows_client.DbUtils;
 using windows_client.Model;
 using System.IO;
+using System.Windows.Media;
 
 namespace windows_client.utils
 {
     public class UI_Utils
     {
+        public static readonly SolidColorBrush whiteBackground = new SolidColorBrush(Color.FromArgb(255, 255, 255, 255));
+        public static readonly SolidColorBrush blackBackground = new SolidColorBrush(Color.FromArgb(0, 0, 0, 0));
+        public static readonly SolidColorBrush textBoxBackground = new SolidColorBrush(Color.FromArgb(255, 238, 238, 236));
+        public static readonly SolidColorBrush smsBackground = new SolidColorBrush(Color.FromArgb(255, 219, 242, 207));
+        public static readonly SolidColorBrush hikeMsgBackground = new SolidColorBrush(Color.FromArgb(255, 177, 224, 251));
+
         private BitmapImage onHikeImage = null;
         private BitmapImage notOnHikeImage = null;
         private BitmapImage defaultAvatarBitmapImage = null;
@@ -27,6 +34,7 @@ namespace windows_client.utils
             sent = new BitmapImage(new Uri("/View/images/ic_sent.png", UriKind.Relative));
             delivered = new BitmapImage(new Uri("/View/images/ic_delivered.png", UriKind.Relative));
             read = new BitmapImage(new Uri("/View/images/ic_read.png", UriKind.Relative));
+            onHikeImage = new BitmapImage(new Uri("/View/images/ic_hike_user.png", UriKind.Relative));
         }
 
         public static UI_Utils Instance
@@ -74,6 +82,22 @@ namespace windows_client.utils
             get
             {
                 return read;
+            }
+        }
+
+        public BitmapImage MessageDeliveredBitmapImage
+        {
+            get
+            {
+                return delivered;
+            }
+        }
+
+        public BitmapImage MessageSentBitmapImage
+        {
+            get
+            {
+                return sent;
             }
         }
 
