6e9c53cb2f9ab0ca0467997ab24093adc548f8de
madhurgarg <mg@bsb.in>
2012-08-21 20:00:13 +0530

    Construct with convmessage. Design changes

--- windows-client/Controls/SentChatBubble.xaml.cs
+++ windows-client/Controls/SentChatBubble.xaml.cs
@@ -10,15 +10,36 @@ using System.Windows.Shapes;
 using System.Windows.Data;
 using windows_client;
 using windows_client.utils;
+using windows_client.Model;
+using System.Windows.Media.Imaging;
 
 namespace windows_client.Controls
 {
     public partial class SentChatBubble : MyChatBubble {
-        public SentChatBubble() {
+        private SolidColorBrush bubbleColor;
+        public SentChatBubble(ConvMessage cm) 
+        {
             // Required to initialize variables
             InitializeComponent();
 
             this.SDRImage.Source = UI_Utils.Instance.MessageReadBitmapImage;
+            this.Text = cm.Message;
+            this.TimeStamp = DateTime.Now;
+            //IsSms is false for group chat
+            if (cm.IsSms)
+            {
+                bubbleColor = UI_Utils.smsBackground;
+            }
+            else
+            {
+                bubbleColor = UI_Utils.hikeMsgBackground;
+            }
+            this.BubblePoint.Fill = bubbleColor;
+            this.BubbleBg.Fill = bubbleColor;
+        }
+        public void SetSDRImage(BitmapImage bm)
+        {
+            this.SDRImage.Source = bm;
         }
     }
 }
