d96a13c2ae3927d8eb2942a07eb5ae7a5f2b827c
Madhur <mg@bsb.in>
2012-09-03 00:20:32 +0530

    Added image in received chat bubble

--- windows-client/Controls/ReceivedChatBubble.xaml.cs
+++ windows-client/Controls/ReceivedChatBubble.xaml.cs
@@ -10,16 +10,27 @@ using System.Windows.Shapes;
 using windows_client.Model;
 using windows_client.utils;
 using System.Collections.Generic;
+using System.Windows.Media.Imaging;
+using System.IO;
 
 namespace windows_client.Controls
 {
     public partial class ReceivedChatBubble : MyChatBubble
     {
+
         public ReceivedChatBubble(ConvMessage cm, Dictionary<string, RoutedEventHandler> contextMenuDictionary)
             : base(cm, contextMenuDictionary)
         {
             // Required to initialize variables
             InitializeComponent();
+            if (cm.FileAttachment != null && cm.FileAttachment.Thumbnail != null)
+            {
+                MemoryStream memStream = new MemoryStream(cm.FileAttachment.Thumbnail);
+                memStream.Seek(0, SeekOrigin.Begin);
+                BitmapImage fileThumbnail = new BitmapImage();
+                fileThumbnail.SetSource(memStream);
+                this.MessageImage.Source = fileThumbnail;
+            }
         }
     }
 }
