548ab7751cf60a89574284a0f932e41189efb5ab
Madhur <mg@bsb.in>
2012-09-06 16:11:04 +0530

    Changes for adding and displaying image of file transfer in sent chat bubble

--- windows-client/Controls/SentChatBubble.xaml.cs
+++ windows-client/Controls/SentChatBubble.xaml.cs
@@ -1,24 +1,16 @@
 ﻿using System;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Documents;
-using System.Windows.Ink;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Animation;
-using System.Windows.Shapes;
-using System.Windows.Data;
-using windows_client;
 using windows_client.utils;
 using windows_client.Model;
 using System.Windows.Media.Imaging;
-using windows_client.View;
-using Microsoft.Phone.Controls;
 using System.Collections.Generic;
+using System.IO;
 
 namespace windows_client.Controls
 {
-    public partial class SentChatBubble : MyChatBubble {
+    public partial class SentChatBubble : MyChatBubble
+    {
         private SolidColorBrush bubbleColor;
         private ConvMessage.State messageState;
         public SentChatBubble(ConvMessage cm, Dictionary<string, RoutedEventHandler> contextMenuDictionary)
@@ -36,7 +28,7 @@ namespace windows_client.Controls
                 bubbleColor = UI_Utils.Instance.HikeMsgBackground;
             }
             switch (cm.MessageStatus)
-            { 
+            {
                 case ConvMessage.State.SENT_CONFIRMED:
                     this.SDRImage.Source = UI_Utils.Instance.MessageSentBitmapImage;
                     break;
@@ -48,21 +40,52 @@ namespace windows_client.Controls
                     break;
                 default:
                     break;
+            }
+            if (cm.HasAttachment)
+            {
+                byte[] imageBytes;
+                Attachment.readFileFromIsolatedStorage(HikeConstants.FILES_BYTE_LOCATION + "/" + 
+                    cm.Msisdn + "/" + Convert.ToString(cm.MessageId), out imageBytes);
 
+                MemoryStream memStream = new MemoryStream(imageBytes);
+                memStream.Seek(0, SeekOrigin.Begin);
+                BitmapImage fileThumbnail = new BitmapImage();
+                fileThumbnail.SetSource(memStream);
+                this.MessageImage.Source = fileThumbnail;
             }
             this.BubblePoint.Fill = bubbleColor;
             this.BubbleBg.Fill = bubbleColor;
         }
+
+        public SentChatBubble(bool onHike, BitmapImage sentImage, long messageId)
+            : base(messageId)
+        {
+            // Required to initialize variables
+            InitializeComponent();
+
+            if (onHike)
+            {
+                bubbleColor = UI_Utils.Instance.HikeMsgBackground;
+            }
+            else
+            {
+                bubbleColor = UI_Utils.Instance.SmsBackground;
+            }
+            this.BubblePoint.Fill = bubbleColor;
+            this.BubbleBg.Fill = bubbleColor;
+            this.MessageImage.Source = sentImage;
+        }
+
+
         public void SetSentMessageStatus(ConvMessage.State msgState)
         {
-
             if ((int)messageState < (int)msgState)
             {
                 messageState = msgState;
                 Deployment.Current.Dispatcher.BeginInvoke(() =>
                 {
                     switch (messageState)
-                    { 
+                    {
                         case ConvMessage.State.SENT_CONFIRMED:
                             this.SDRImage.Source = UI_Utils.Instance.MessageSentBitmapImage;
                             break;
