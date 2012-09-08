9657ea9a305f55aef4f5e50bc20e6ea5735cf596
Madhur <mg@bsb.in>
2012-09-08 17:21:49 +0530

    Added enum for file transfer state

--- windows-client/Model/Attachment.cs
+++ windows-client/Model/Attachment.cs
@@ -1,14 +1,28 @@
 ﻿using System.IO.IsolatedStorage;
+using ProtoBuf;
 
 namespace windows_client.Model
 {
+    [ProtoContract]
+
     public class Attachment
     {
         private string _fileKey;
         private string _fileName;
         private string _contentType;
         private byte[] _thumbnail;
+        private AttachmentState _fileState;
 
+
+        public enum AttachmentState
+        {
+            FAILED_OR_NOT_STARTED = 0,  /* message sent to server */
+            STARTED, /* message could not be sent, manually retry */
+            COMPLETED, /* message received by server */
+            CANCELED
+        }
+
+        [ProtoMember(1)]
         public string FileKey
         {
             get
@@ -24,6 +38,7 @@ namespace windows_client.Model
             }
         }
 
+        [ProtoMember(2)]
         public string FileName
         {
             get
@@ -39,6 +54,7 @@ namespace windows_client.Model
             }
         }
 
+        [ProtoMember(3)]
         public string ContentType
         {
             get
@@ -54,6 +70,7 @@ namespace windows_client.Model
             }
         }
 
+        [ProtoMember(4)]
         public byte[] Thumbnail
         {
             get
@@ -69,19 +86,46 @@ namespace windows_client.Model
             }
         }
 
+        [ProtoMember(5)]
+        public AttachmentState FileState
+        {
+            get
+            {
+                return _fileState;
+            }
+            set
+            {
+                if (_fileState != value)
+                {
+                    _fileState = value;
+                }
+            }
+
+        }
+
         //newly received messages
-        public Attachment(string fileName, string fileKey, byte[] thumbnailBytes, string contentType)
+        public Attachment(string fileName, string fileKey, byte[] thumbnailBytes, string contentType, AttachmentState attachmentState)
         {
             this.FileName = fileName;
             this.FileKey = fileKey;
             this.Thumbnail = thumbnailBytes;
             this.ContentType = contentType;
+            this.FileState = attachmentState;
         }
 
-        public Attachment(string fileName, byte[] thumbnailBytes)
+        //public Attachment(string fileName, string fileKey, string contentType)
+        //{
+        //    this.FileName = fileName;
+        //    this.FileKey = fileKey;
+        //    this.ContentType = contentType;
+        //}
+
+
+        public Attachment(string fileName, byte[] thumbnailBytes, AttachmentState attachmentState)
         {
             this.FileName = fileName;
             this.Thumbnail = thumbnailBytes;
+            this.FileState = attachmentState;
         }
 
         public static string[] getAttachmentFiles(string msisdn)
