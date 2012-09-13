6a989d4307cf5fcb3eb312e6717489ea7ba60406
Madhur <mg@bsb.in>
2012-09-13 18:19:04 +0530

    Moving all UI from xaml to code file

--- windows-client/Controls/SentChatBubble.xaml.cs
+++ windows-client/Controls/SentChatBubble.xaml.cs
@@ -1,13 +1,12 @@
-﻿using System;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Media;
 using windows_client.utils;
 using windows_client.Model;
 using System.Windows.Media.Imaging;
-using System.Collections.Generic;
 using System.IO;
-using windows_client.DbUtils;
-using Microsoft.Phone.Controls;
+using System.Windows.Controls;
+using System.Windows.Shapes;
+using System.Windows.Data;
 
 namespace windows_client.Controls
 {
@@ -153,20 +152,108 @@ namespace windows_client.Controls
             });
         }
 
+        private Grid attachment;
+        public Image MessageImage;
+        private Image PlayIcon;
+        private ProgressBar uploadProgress;
+        private LinkifiedTextBox MessageText;
+        private TextBlock TimeStampBlock;
+        private Rectangle BubbleBg;
+        private Image SDRImage;
+
+        private static Thickness imgMargin = new Thickness(12, 12, 12, 0);
+        private static Thickness progressMargin = new Thickness(0, 5, 0, 0);
+        private static Thickness messageTextMargin = new Thickness(0, 6, 0, 0);
+        private static Thickness timeStampBlockMargin = new Thickness(12, 0, 12, 6);
+        private readonly SolidColorBrush progressColor = new SolidColorBrush(Color.FromArgb(255, 51, 51, 51));
+        private static Thickness sdrImageMargin = new Thickness(0, 0, 10, 0);
+
         private void initializeBasedOnState(bool hasAttachment, string contentType)
         {
+            BubbleBg = new Rectangle();
+            BubbleBg.Fill = UI_Utils.Instance.TextBoxBackground;
+            Grid.SetRowSpan(BubbleBg, 2);
+            Grid.SetColumn(BubbleBg, 1);
+            wrapperGrid.Children.Add(BubbleBg);
+
             if (hasAttachment)
             {
-                this.MessageText.Visibility = Visibility.Collapsed;
+                attachment = new Grid();
+                RowDefinition r1 = new RowDefinition();
+                r1.Height = GridLength.Auto;
+                RowDefinition r2 = new RowDefinition();
+                r2.Height = GridLength.Auto;
+                attachment.RowDefinitions.Add(r1);
+                attachment.RowDefinitions.Add(r2);
+                Grid.SetRow(attachment, 0);
+                Grid.SetColumn(attachment, 1);
+                wrapperGrid.Children.Add(attachment);
+
+                MessageImage = new Image();
+                MessageImage.MaxWidth = 180;
+                MessageImage.MaxHeight = 180;
+                MessageImage.HorizontalAlignment = HorizontalAlignment.Right;
+                MessageImage.Margin = imgMargin;
+                if (contentType.Contains("audio"))
+                    this.MessageImage.Source = UI_Utils.Instance.AudioAttachment;
+
+                Grid.SetRow(MessageImage, 0);
+                attachment.Children.Add(MessageImage);
+
                 if (contentType.Contains("video") || contentType.Contains("audio"))
                 {
-                    if (contentType.Contains("audio"))
-                        this.MessageImage.Source = UI_Utils.Instance.AudioAttachment;
-                    PlayIcon.Visibility = Visibility.Visible;
+
+                    PlayIcon = new Image();
+                    PlayIcon.MaxWidth = 43;
+                    PlayIcon.MaxHeight = 42;
+                    PlayIcon.Source = UI_Utils.Instance.PlayIcon;
+                    PlayIcon.HorizontalAlignment = HorizontalAlignment.Center;
+                    PlayIcon.VerticalAlignment = VerticalAlignment.Center;
+
+                    PlayIcon.Margin = imgMargin;
+                    Grid.SetRow(PlayIcon, 0);
+                    attachment.Children.Add(PlayIcon);
                 }
+
+                //    <ProgressBar Grid.Row="1" x:Name="uploadProgress" Margin="0,5,0,0" Grid.Column="1" Height="8" Background="Transparent" Foreground="#333333" Minimum="0" Maximum="100"></ProgressBar>
+                uploadProgress = new ProgressBar();
+                uploadProgress.Height = 10;
+                uploadProgress.Foreground = progressColor;
+                uploadProgress.Minimum = 0;
+                uploadProgress.MaxHeight = 100;
+                Grid.SetRow(uploadProgress, 1);
+                attachment.Children.Add(uploadProgress);
             }
             else
-                this.attachment.Visibility = Visibility.Collapsed;
+            {
+                MessageText = new LinkifiedTextBox();
+                MessageText.Width = 340;
+                MessageText.Foreground = progressColor;
+                MessageText.Margin = messageTextMargin;
+                Binding messageTextBinding = new Binding("Text");
+                MessageText.SetBinding(LinkifiedTextBox.TextProperty, messageTextBinding);
+                Grid.SetRow(MessageText, 0);
+                Grid.SetColumn(MessageText, 1);
+                wrapperGrid.Children.Add(MessageText);
+            }
+
+            SDRImage = new Image();
+            SDRImage.Margin = sdrImageMargin;
+            SDRImage.Height = 20;
+            Grid.SetRowSpan(SDRImage, 2);
+            Grid.SetColumn(SDRImage, 0);
+            wrapperGrid.Children.Add(SDRImage);
+
+            TimeStampBlock = new TextBlock();
+            TimeStampBlock.HorizontalAlignment = System.Windows.HorizontalAlignment.Right;
+            TimeStampBlock.FontSize = 18;
+            TimeStampBlock.Foreground = progressColor;
+            TimeStampBlock.Text = TimeStamp;
+            TimeStampBlock.Margin = timeStampBlockMargin;
+            Grid.SetRow(TimeStampBlock, 1);
+            Grid.SetColumn(TimeStampBlock, 1);
+            wrapperGrid.Children.Add(TimeStampBlock);
+
         }
     }
 }
