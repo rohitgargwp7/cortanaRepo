fcdfa7a8443c16df7f2fc67c38780b86913eeaa9
Madhur <mg@bsb.in>
2012-09-23 17:01:49 +0530

    Adding images for three pivots

--- windows-client/View/Walkthrough.xaml.cs
+++ windows-client/View/Walkthrough.xaml.cs
@@ -10,14 +10,50 @@ using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
 
 namespace windows_client.View
 {
     public partial class Walkthrough : PhoneApplicationPage
     {
+        private ApplicationBar appBar;
+        ApplicationBarIconButton nextIconButton;
+
         public Walkthrough()
         {
             InitializeComponent();
+
+            appBar = new ApplicationBar();
+            appBar.Mode = ApplicationBarMode.Default;
+            appBar.Opacity = 1;
+            appBar.IsVisible = true;
+            appBar.IsMenuEnabled = false;
+
+            nextIconButton = new ApplicationBarIconButton();
+            nextIconButton.IconUri = new Uri("/View/images/icon_next.png", UriKind.Relative);
+            nextIconButton.Text = "Done";
+            nextIconButton.Click += new EventHandler(doneBtn_Click);
+            nextIconButton.IsEnabled = false;
+            appBar.Buttons.Add(nextIconButton);
+            walkThrough.ApplicationBar = appBar;
+        }
+
+        private void doneBtn_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void walkThroughPvt_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            switch (walkThroughPvt.SelectedIndex)
+            {
+                case 0:
+                case 1:
+                    nextIconButton.IsEnabled = false;
+                    break;
+                case 2:
+                    nextIconButton.IsEnabled = true;
+                    break;
+            }
         }
     }
 }
