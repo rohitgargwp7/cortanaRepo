2f5d3c1a246588e3cfdd03ee849034bd4bb59ac3
Madhur <mg@bsb.in>
2012-09-25 19:56:37 +0530

    UI chnages. Theme support

--- windows-client/View/FreeSMS.xaml.cs
+++ windows-client/View/FreeSMS.xaml.cs
@@ -10,13 +10,14 @@ using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using Microsoft.Phone.Controls;
+using windows_client.utils;
 
 namespace windows_client.View
 {
     public partial class FreeSMS : PhoneApplicationPage
     {
         private readonly SolidColorBrush rectangleColor = new SolidColorBrush(Color.FromArgb(255, 51, 51, 51));
-        private readonly Thickness box4Margin = new Thickness(5,5,5,5);
+        private readonly Thickness box4Margin = new Thickness(5, 5, 5, 5);
 
         public FreeSMS()
         {
@@ -26,16 +27,17 @@ namespace windows_client.View
 
         private void initpageBasedOnState()
         {
-            int creditsRemaining = (int)App.appSettings[App.SMS_SETTING];
-//            int creditsRemaining = 8796;
+//            int creditsRemaining = (int)App.appSettings[App.SMS_SETTING];
+            int creditsRemaining = 8796;
             TextBlock t3 = null;
+            Rectangle r3 = null;
             if (creditsRemaining > 999)
             {
                 ColumnDefinition c4 = new ColumnDefinition();
                 c4.Width = GridLength.Auto;
                 SMSCounterGrid.ColumnDefinitions.Add(c4);
-                
-                Rectangle r3 = new Rectangle();
+
+                r3 = new Rectangle();
                 r3.Fill = rectangleColor;
                 r3.Margin = box4Margin;
                 r3.Width = 40;
@@ -51,7 +53,7 @@ namespace windows_client.View
 
             creditsRemaining %= 10000;
             string strCreditsWithZeroes;
-            if(creditsRemaining > 999)
+            if (creditsRemaining > 999)
                 strCreditsWithZeroes = creditsRemaining.ToString("0000");
             else
                 strCreditsWithZeroes = creditsRemaining.ToString("000");
@@ -59,8 +61,26 @@ namespace windows_client.View
             t0.Text = strCreditsWithZeroes[0].ToString();
             t1.Text = strCreditsWithZeroes[1].ToString();
             t2.Text = strCreditsWithZeroes[2].ToString();
-            if(t3!=null)
+            if (t3 != null)
                 t3.Text = strCreditsWithZeroes[3].ToString();
+
+            if (Utils.isDarkTheme())
+            {
+                this.FreeSMSGrid.Background = new SolidColorBrush(Color.FromArgb(255, 0x1f, 0x1f, 0x1f));
+                t0.Foreground = t1.Foreground = t2.Foreground = UI_Utils.Instance.Black;
+                r0.Fill = r1.Fill = r2.Fill = UI_Utils.Instance.White;
+            }
+            else
+            {
+                this.FreeSMSGrid.Background = new SolidColorBrush(Color.FromArgb(255, 0xf6, 0xf6, 0xf6));
+                t0.Foreground = t1.Foreground = t2.Foreground = UI_Utils.Instance.White;
+                r0.Fill = r1.Fill = r2.Fill = new SolidColorBrush(Color.FromArgb(255, 0x2f, 0x2f, 0x2f));
+            }
+            if (t3 != null)
+                t3.Foreground = t0.Foreground;
+            if (r3 != null)
+                r3.Fill = r0.Fill;
+
 
         }
     }
