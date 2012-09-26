d85a75d371f14e4ec860bd147be367d7b510942e
Madhur <mg@bsb.in>
2012-09-26 23:40:43 +0530

    Made help screen

--- windows-client/View/Help.xaml.cs
+++ windows-client/View/Help.xaml.cs
@@ -10,38 +10,44 @@ using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using Microsoft.Phone.Controls;
-using Microsoft.Phone.Shell;
+using Microsoft.Phone.Tasks;
 
 namespace windows_client.View
 {
-
     public partial class Help : PhoneApplicationPage
     {
-        private ApplicationBar appBar;
-        ApplicationBarIconButton nextIconButton;
         public Help()
         {
             InitializeComponent();
-            appBar = new ApplicationBar();
-            appBar.Mode = ApplicationBarMode.Default;
-            appBar.Opacity = 1;
-            appBar.IsVisible = true;
-//            appBar.IsMenuEnabled = false;
-
-            nextIconButton = new ApplicationBarIconButton();
-            nextIconButton.IconUri = new Uri("/View/images/icon_tick.png", UriKind.Relative);
-            nextIconButton.Text = "accept";
-            nextIconButton.Click += new EventHandler(doneBtn_Click);
-            nextIconButton.IsEnabled = true;
-            appBar.Buttons.Add(nextIconButton);
-            help.ApplicationBar = appBar;
         }
 
-        private void doneBtn_Click(object sender, EventArgs e)
+        private void FAQs_Tap(object sender, System.Windows.Input.GestureEventArgs e)
         {
-            NavigationService.Navigate(new Uri("/View/WelcomePage.xaml", UriKind.Relative));
+            NavigationService.Navigate(new Uri("/View/FAQTerms.xaml?page=faq", UriKind.Relative));
 
         }
 
+        private void ContactUs_Tap(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            WebBrowserTask wbt = new WebBrowserTask();
+            wbt.Uri = new Uri(Uri.EscapeUriString(HikeConstants.contactUsLink), UriKind.RelativeOrAbsolute);
+            wbt.Show();
+        }
+
+        private void Legal_Tap(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/View/FAQTerms.xaml?page=legal", UriKind.Relative));
+        }
+
+        private void Updates_Tap(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+
+        }
+        
+        private void Walkthrough_Tap(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/View/Walkthrough.xaml", UriKind.Relative));
+
+        }
     }
 }
