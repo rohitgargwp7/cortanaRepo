758d5bd1897363ec82a17d16a21601fbc5368217
Gautam Kanwar <gk@bsb.in>
2012-10-01 21:57:49 +0530

    Added invite friends support.

--- windows-client/View/Invite.xaml.cs
+++ windows-client/View/Invite.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using Microsoft.Phone.Controls;
+using Microsoft.Phone.Tasks;
 
 namespace windows_client.View
 {
@@ -22,17 +23,35 @@ namespace windows_client.View
 
         private void Social_Tap(object sender, System.Windows.Input.GestureEventArgs e)
         {
-
+            string inviteToken = "";
+            App.appSettings.TryGetValue<string>(HikeConstants.INVITE_TOKEN, out inviteToken);
+            string inviteMsg = string.Format(App.invite_message, inviteToken);
+            ShareLinkTask shareLinkTask = new ShareLinkTask();
+            shareLinkTask.LinkUri = new Uri("http://get.hike.in/" + inviteToken, UriKind.Absolute);
+            shareLinkTask.Title = "Hike, Free messaging for life.";
+            shareLinkTask.Message = inviteMsg;
+            shareLinkTask.Show();
         }
 
         private void Email_Tap(object sender, System.Windows.Input.GestureEventArgs e)
         {
-
+            string inviteToken = "";
+            App.appSettings.TryGetValue<string>(HikeConstants.INVITE_TOKEN, out inviteToken);
+            string inviteMsg = string.Format(App.invite_message, inviteToken);
+            EmailComposeTask f5EmailCompose = new EmailComposeTask();
+            f5EmailCompose.Subject = "Hike, Free messaging for life.";
+            f5EmailCompose.Body = inviteMsg;
+            f5EmailCompose.Show();
         }
 
         private void Messaging_Tap(object sender, System.Windows.Input.GestureEventArgs e)
         {
-
+            string inviteToken = "";
+            App.appSettings.TryGetValue<string>(HikeConstants.INVITE_TOKEN, out inviteToken);
+            string inviteMsg = string.Format(App.invite_message, inviteToken);
+            SmsComposeTask sms = new Microsoft.Phone.Tasks.SmsComposeTask();
+            sms.Body = inviteMsg;
+            sms.Show();
         }
     }
 }
