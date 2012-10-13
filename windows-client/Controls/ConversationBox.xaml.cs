761eb6a1bf7377d4b738ef35246ca99cdd165a82
Madhur <mg@bsb.in>
2012-10-14 00:36:37 +0530

    Added variable for message state in conversation box

--- windows-client/Controls/ConversationBox.xaml.cs
+++ windows-client/Controls/ConversationBox.xaml.cs
@@ -7,13 +7,15 @@ using System.Text.RegularExpressions;
 using System;
 using Microsoft.Phone.Tasks;
 using System.Windows.Media.Imaging;
+using windows_client.Model;
 
 namespace windows_client.Controls
 {
     public partial class ConversationBox : UserControl
     {
-        public ConversationBox(BitmapImage profileImage, string userName, string lastMessage, long timeStamp, 
-            bool isSentMessage) //set it true is we have to show SDR, set false for event notification messages
+        public ConversationBox(BitmapImage profileImage, string userName, string lastMessage, long timeStamp,
+            bool isSentMessage,  //set it true is we have to show SDR, set false for event notification messages
+            ConvMessage.State messageState)
         {
             this.profileImage.Source = profileImage;
             this.userName.Text = userName;
