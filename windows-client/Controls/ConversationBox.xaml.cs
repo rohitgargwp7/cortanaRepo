36daba6a6b846f1c4c99a06f483bddee905b88fd
Madhur <mg@bsb.in>
2012-10-13 23:48:43 +0530

    Added custom control for conversations

---
+++ windows-client/Controls/ConversationBox.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using windows_client.utils;
+using System.Windows.Media;
+using System.Text.RegularExpressions;
+using System;
+using Microsoft.Phone.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace windows_client.Controls
+{
+    public partial class ConversationBox : UserControl
+    {
+        public ConversationBox(BitmapImage profileImage, string userName, string lastMessage, long timeStamp)
+        {
+            this.profileImage.Source = profileImage;
+            this.userName.Text = userName;
+            this.lastMessage.Text = lastMessage;
+            this.timestamp.Text = TimeUtils.getTimeString(timeStamp);
+        }
+    }
+}
