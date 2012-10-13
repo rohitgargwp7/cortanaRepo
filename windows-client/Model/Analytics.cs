2f5e8fe20bf22ff1f3ed843cacb02e79c60ae635
Madhur <mg@bsb.in>
2012-10-13 19:23:06 +0530

    Added functions to save and read from isolated storage

--- windows-client/Model/Analytics.cs
+++ windows-client/Model/Analytics.cs
@@ -1,31 +1,30 @@
 ﻿using System;
-using System.Net;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Documents;
-using System.Windows.Ink;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Animation;
-using System.Windows.Shapes;
 using System.Collections.Generic;
 using Newtonsoft.Json.Linq;
+using windows_client.Misc;
+using System.IO;
+using System.IO.IsolatedStorage;
 
 namespace windows_client.Model
 {
-    public class Analytics
+    public class Analytics : IBinarySerializable
     {
-        //coSc = conversations screen
+        //co = conversations screen
 
         public static readonly string DELETE_ALL_CHATS = "coDelAll";
         public static readonly string COMPOSE = "coCom";
 
-        //prSc = profile screen
+        //pr = profile screen
         public static readonly string FREE_SMS = "prFrS";
         public static readonly string INVITE = "prInv";
         public static readonly string SETTINGS = "prSet";
         public static readonly string HELP = "prHlp";
         public static readonly string EDIT_PROFILE = "prEdtPr";
+
+        //in = invite Screen
+        public static readonly string INVITE_SOCIAL = "inSo";
+        public static readonly string INVITE_EMAIL = "inEm";
+        public static readonly string INVITE_MESSAGE = "inMsg";
 
         private Dictionary<string, int> eventMap = new Dictionary<string, int>();
 
@@ -41,13 +40,15 @@ namespace windows_client.Model
                     lock (syncRoot)
                     {
                         if (instance == null)
+                        {
                             instance = new Analytics();
+                            instance.readObject();
+                        }
                     }
                 }
                 return instance;
             }
         }
-
 
         public void addEvent(string eventKey)
         {
@@ -63,7 +64,7 @@ namespace windows_client.Model
         {
             JObject eventsData = new JObject();
             eventsData["tag"] = "mob";
-            foreach(KeyValuePair<string,int> entry in eventMap)
+            foreach (KeyValuePair<string, int> entry in eventMap)
             {
                 if (entry.Value != 0)
                 {
@@ -76,5 +77,69 @@ namespace windows_client.Model
             return serializedJson;
         }
 
+        public void Write(BinaryWriter writer)
+        {
+            writer.Write(eventMap.Count);
+            foreach (KeyValuePair<string, int> entry in eventMap)
+            {
+                if (entry.Value > 0)
+                {
+                    writer.WriteString(entry.Key);
+                    writer.Write(entry.Value);
+                }
+            }
+        }
+
+        public void Read(BinaryReader reader)
+        {
+            int count = reader.ReadInt32();
+            string key;
+            int value = -1;
+            for (int i = 0; i < count; i++)
+            {
+                key = reader.ReadString();
+                value = reader.ReadInt32();
+                if (!String.IsNullOrEmpty(key) && value > 0)
+                    eventMap[key] = value;
+            }
+        }
+
+        public void saveObject()
+        {
+            string filePath = HikeConstants.ANALYTICS_OBJECT_DIRECTORY + "/" + HikeConstants.ANALYTICS_OBJECT_FILE;
+            using (IsolatedStorageFile store = IsolatedStorageFile.GetUserStoreForApplication()) // grab the storage
+            {
+                if (store.FileExists(filePath))
+                {
+                    store.DeleteFile(filePath);
+                }
+                using (var file = store.OpenFile(filePath, FileMode.Create, FileAccess.Write))
+                {
+                    using (var writer = new BinaryWriter(file))
+                    {
+                        this.Write(writer);
+                    }
+                }
+            }
+        }
+
+        private void readObject()
+        { 
+            string filePath = HikeConstants.ANALYTICS_OBJECT_DIRECTORY + "/" + HikeConstants.ANALYTICS_OBJECT_FILE;
+            using (IsolatedStorageFile store = IsolatedStorageFile.GetUserStoreForApplication()) // grab the storage
+            {
+                if (!store.FileExists(filePath))
+                {
+                    return;
+                }
+                using (var file = store.OpenFile(filePath, FileMode.Open, FileAccess.Read))
+                {
+                    using (var reader = new BinaryReader(file))
+                    {
+                        this.Read(reader);
+                    }
+                }
+            }        
+        }
     }
 }
