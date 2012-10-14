a52917d563d0cd8fa87e3b18498f873c4467c527
Madhur <mg@bsb.in>
2012-10-14 10:08:55 +0530

    1) Added a few more constructors 2) Property for avatar image ad message status

--- windows-client/Controls/ConversationBox.xaml.cs
+++ windows-client/Controls/ConversationBox.xaml.cs
@@ -13,14 +13,68 @@ namespace windows_client.Controls
 {
     public partial class ConversationBox : UserControl
     {
-        public ConversationBox(BitmapImage profileImage, string userName, string lastMessage, long timeStamp,
-            bool isSentMessage,  //set it true is we have to show SDR, set false for event notification messages
+        private ConvMessage.State _messageStatus;
+        private byte[] _avatar;
+
+        //GK add message id as private member if required
+
+        public ConversationBox(byte[] avatar, string userName, string lastMessage, long timeStamp,
+            bool isNotification,  //set it true for event notification messages
             ConvMessage.State messageState)
         {
-            this.profileImage.Source = profileImage;
+            this._avatar = avatar;
             this.userName.Text = userName;
             this.lastMessage.Text = lastMessage;
             this.timestamp.Text = TimeUtils.getTimeString(timeStamp);
+            this._messageStatus = messageState;
         }
+
+        public ConversationBox(string userName, string lastMessage, long timeStamp,
+            ConvMessage.State messageState)
+            :this(null, userName, lastMessage, timeStamp, false, messageState)
+        { 
+        
+        }
+
+        public ConversationBox(byte[] avatar, string userName, string lastMessage, long timeStamp,
+            ConvMessage.State messageState)
+            : this(avatar, userName, lastMessage, timeStamp, false, messageState)
+        {
+
+        }
+
+
+        //this property is used for showing ~,S,D,R & dor for unread messages
+        public ConvMessage.State MessageStatus
+        {
+            get
+            {
+                return _messageStatus;
+            }
+            set
+            {
+                if (_messageStatus != value)
+                {
+                    _messageStatus = value;
+                }
+            }
+        }
+
+        //set this on updation of profile image
+        public byte[] Avatar
+        {
+            get
+            {
+                return _avatar;
+            }
+            set
+            {
+                if (_avatar != value)
+                {
+                    _avatar = value;
+                }
+            }
+        }
+
     }
 }
