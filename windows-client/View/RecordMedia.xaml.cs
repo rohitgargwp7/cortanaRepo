a056fdbb7f79e4099e24498cd0bb1bdf8d91684b
Madhur <mg@bsb.in>
2012-10-16 14:52:00 +0530

    Record audio page

--- windows-client/View/RecordMedia.xaml.cs
+++ windows-client/View/RecordMedia.xaml.cs
@@ -29,6 +29,9 @@ namespace windows_client.View
         private byte[] _buffer;
         private TimeSpan _duration;
 
+        private int maxPlayDuration = 120;
+        private int state = 0;
+
 
         public RecordMedia()
         {
@@ -55,8 +58,6 @@ namespace windows_client.View
             progressTimer = new DispatcherTimer();
             progressTimer.Interval = TimeSpan.FromSeconds(1);
             progressTimer.Tick += new EventHandler(showProgress);
-
-
         }
 
         void dt_Tick(object sender, EventArgs e)
@@ -70,75 +71,43 @@ namespace windows_client.View
                 {
                     // Audio has finished playing
                     soundIsPlaying = false;
-
-                    // Update the UI to reflect that the 
-                    // sound has stopped playing
-                    SetButtonStates(true, true, false);
-                    UserHelp.Text = "press play\nor record";
-                    StatusImage.Source = blankImage;
+                    stop();
                 }
             }
         }
 
-        /// <summary>
-        /// The Microphone.BufferReady event handler.
-        /// Gets the audio data from the microphone and stores it in a buffer,
-        /// then writes that buffer to a stream for later playback.
-        /// Any action in this event handler should be quick!
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
         void microphone_BufferReady(object sender, EventArgs e)
         {
             // Retrieve audio data
             microphone.GetData(buffer);
-
-            // Store the audio data in a stream
             stream.Write(buffer, 0, buffer.Length);
         }
 
-        /// <summary>
-        /// Handles the Click event for the record button.
-        /// Sets up the microphone and data buffers to collect audio data,
-        /// then starts the microphone. Also, updates the UI.
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void recordButton_Click(object sender, EventArgs e)
+        private void record()
         {
             // Get audio data in 1/2 second chunks
             microphone.BufferDuration = TimeSpan.FromMilliseconds(500);
-
             // Allocate memory to hold the audio data
             buffer = new byte[microphone.GetSampleSizeInBytes(microphone.BufferDuration)];
-
             // Set the stream back to zero in case there is already something in it
             stream.SetLength(0);
-
             // Start recording
             microphone.Start();
-
-            SetButtonStates(false, false, true);
-            UserHelp.Text = "record";
-            StatusImage.Source = microphoneImage;
-
+            timeBar.Opacity = 1;
             progressTimer.Start();
-            message.Text = "RECORDGING";
-
-
+            statusImage.Source = new BitmapImage(new Uri("/images/icon_record.png", UriKind.Relative));
+            message.Text = "RECORDING";
         }
+
         void showProgress(object sender, EventArgs e)
         {
             runningSeconds++;
-            runningTime.Text = Convert.ToString(runningSeconds);
+            runningTime.Text = formatTime(runningSeconds);
+            if (runningSeconds >= 120)
+                stop();
         }
-        /// <summary>
-        /// Handles the Click event for the stop button.
-        /// Stops the microphone from collecting audio and updates the UI.
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void stopButton_Click(object sender, EventArgs e)
+
+        private void stop()
         {
             if (microphone.State == MicrophoneState.Started)
             {
@@ -148,50 +117,33 @@ namespace windows_client.View
             }
             else if (soundInstance.State == SoundState.Playing)
             {
-                // In PLAY mode, user clicked the 
-                // stop button to end playing back
                 soundInstance.Stop();
             }
-
-            SetButtonStates(true, true, false);
-            UserHelp.Text = "ready";
-            StatusImage.Source = blankImage;
-
+            timeBar.Opacity = 0;
+            maxPlayDuration = runningSeconds;
             runningSeconds = 0;
             message.Text = "TAP TO PLAY";
+            statusImage.Source = new BitmapImage(new Uri("/images/icon_play.png", UriKind.Relative));
             progressTimer.Stop();
         }
 
-        /// <summary>
-        /// Handles the Click event for the play button.
-        /// Plays the audio collected from the microphone and updates the UI.
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void playButton_Click(object sender, EventArgs e)
+        private void play()
         {
             if (stream.Length > 0)
             {
-                // Update the UI to reflect that
-                // sound is playing
-                SetButtonStates(false, false, true);
-                UserHelp.Text = "play";
-                StatusImage.Source = speakerImage;
-
                 // Play the audio in a new thread so the UI can update.
                 Thread soundThread = new Thread(new ThreadStart(playSound));
                 soundThread.Start();
-                
             }
+            timeBar.Opacity = 1;
             runningSeconds = 0;
             progressTimer.Start();
             message.Text = "PLAYING";
+            statusImage.Source = new BitmapImage(new Uri("/images/icon_play.png", UriKind.Relative));
+
+            maxPlayingTime.Text = " / " + formatTime(maxPlayDuration);
         }
 
-        /// <summary>
-        /// Plays the audio using SoundEffectInstance 
-        /// so we can monitor the playback status.
-        /// </summary>
         private void playSound()
         {
             // Play audio using SoundEffectInstance so we can monitor it's State 
@@ -202,17 +154,42 @@ namespace windows_client.View
             soundInstance.Play();
         }
 
-        /// <summary>
-        /// Helper method to change the IsEnabled property for the ApplicationBarIconButtons.
-        /// </summary>
-        /// <param name="recordEnabled">New state for the record button.</param>
-        /// <param name="playEnabled">New state for the play button.</param>
-        /// <param name="stopEnabled">New state for the stop button.</param>
-        private void SetButtonStates(bool recordEnabled, bool playEnabled, bool stopEnabled)
+        private string formatTime(int seconds)
         {
-            (ApplicationBar.Buttons[0] as ApplicationBarIconButton).IsEnabled = recordEnabled;
-            (ApplicationBar.Buttons[1] as ApplicationBarIconButton).IsEnabled = playEnabled;
-            (ApplicationBar.Buttons[2] as ApplicationBarIconButton).IsEnabled = stopEnabled;
+            int minute = seconds / 60;
+            int secs = seconds % 60;
+            return minute.ToString("00") + ":" + secs.ToString("00");
+        }
+
+        private void Record_Tap(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            switch (state)
+            {
+                case 0:
+                    record();
+                    state = 1;
+                    break;
+                case 1:
+                    stop();
+                    state = 2;
+                    break;
+                case 2:
+                    play();
+                    break;
+            }
+        }
+
+        private void cancel_Click(object sender, EventArgs e)
+        {
+            state = 0;
+            maxPlayDuration = 120;
+            message.Text = "TAP ICON TO RECORD";
+            statusImage.Source = new BitmapImage(new Uri("/images/icon_record.png", UriKind.Relative));
+        }
+
+        private void send_Click(object sender, EventArgs e)
+        {
+
         }
 
     }
