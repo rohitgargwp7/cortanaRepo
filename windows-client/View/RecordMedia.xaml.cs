09f80671671f7185543f88f98f6c97a12c3c3bd9
Madhur <mg@bsb.in>
2012-10-17 15:48:22 +0530

    Convert recorded audio stream to wave format

--- windows-client/View/RecordMedia.xaml.cs
+++ windows-client/View/RecordMedia.xaml.cs
@@ -78,8 +78,8 @@ namespace windows_client.View
             //add icon for cancel
             cancelIconButton = new ApplicationBarIconButton();
             cancelIconButton.IconUri = new Uri("/View/images/icon_cross.png", UriKind.Relative);
-            cancelIconButton.Text = "cancel";
-            cancelIconButton.Click += new EventHandler(send_Click);
+            cancelIconButton.Text = "refresh";
+            cancelIconButton.Click += new EventHandler(refresh_Click);
             cancelIconButton.IsEnabled = true;
             appBar.Buttons.Add(cancelIconButton);
 
@@ -125,6 +125,9 @@ namespace windows_client.View
             buffer = new byte[microphone.GetSampleSizeInBytes(microphone.BufferDuration)];
             // Set the stream back to zero in case there is already something in it
             stream.SetLength(0);
+
+            WriteWavHeader(stream, microphone.SampleRate);
+
             // Start recording
             microphone.Start();
             timeBar.Opacity = 1;
@@ -149,6 +152,7 @@ namespace windows_client.View
                 // In RECORD mode, user clicked the 
                 // stop button to end recording
                 microphone.Stop();
+                UpdateWavHeader(stream);
             }
             else if (soundInstance.State == SoundState.Playing)
             {
@@ -201,32 +205,103 @@ namespace windows_client.View
             switch (state)
             {
                 case 0:
-                    record();
                     state = 1;
+                    record();
                     break;
                 case 1:
-                    stop();
                     state = 2;
+                    stop();
                     break;
                 case 2:
-                    play();
                     state = 1;
+                    play();
                     break;
             }
         }
 
-        private void cancel_Click(object sender, EventArgs e)
+        private void refresh_Click(object sender, EventArgs e)
         {
-            state = 0;
+            if (state > 0)
+                stop();
             maxPlayDuration = 120;
             message.Text = "TAP ICON TO RECORD";
             statusImage.Source = recordIcon;
+            state = 0;
         }
 
         private void send_Click(object sender, EventArgs e)
         {
-            PhoneApplicationService.Current.State[HikeConstants.AUDIO_RECORDED] = stream.ToArray();
-            NavigationService.GoBack();
+            if (stream != null)
+            {
+                byte[] audioBytes = stream.ToArray();
+                if (audioBytes != null && audioBytes.Length > 0)
+                {
+                    PhoneApplicationService.Current.State[HikeConstants.AUDIO_RECORDED] = stream.ToArray();
+                    NavigationService.GoBack();
+                }
+            }
+        }
+
+        public void WriteWavHeader(Stream stream, int sampleRate)
+        {
+            const int bitsPerSample = 16;
+            const int bytesPerSample = bitsPerSample / 8;
+            var encoding = System.Text.Encoding.UTF8;
+            // ChunkID Contains the letters "RIFF" in ASCII form (0x52494646 big-endian form).
+            stream.Write(encoding.GetBytes("RIFF"), 0, 4);
+
+            // NOTE this will be filled in later
+            stream.Write(BitConverter.GetBytes(0), 0, 4);
+
+            // Format Contains the letters "WAVE"(0x57415645 big-endian form).
+            stream.Write(encoding.GetBytes("WAVE"), 0, 4);
+
+            // Subchunk1ID Contains the letters "fmt " (0x666d7420 big-endian form).
+            stream.Write(encoding.GetBytes("fmt "), 0, 4);
+
+            // Subchunk1Size 16 for PCM.  This is the size of therest of the Subchunk which follows this number.
+            stream.Write(BitConverter.GetBytes(16), 0, 4);
+
+            // AudioFormat PCM = 1 (i.e. Linear quantization) Values other than 1 indicate some form of compression.
+            stream.Write(BitConverter.GetBytes((short)1), 0, 2);
+
+            // NumChannels Mono = 1, Stereo = 2, etc.
+            stream.Write(BitConverter.GetBytes((short)1), 0, 2);
+
+            // SampleRate 8000, 44100, etc.
+            stream.Write(BitConverter.GetBytes(sampleRate), 0, 4);
+
+            // ByteRate =  SampleRate * NumChannels * BitsPerSample/8
+            stream.Write(BitConverter.GetBytes(sampleRate * bytesPerSample), 0, 4);
+
+            // BlockAlign NumChannels * BitsPerSample/8 The number of bytes for one sample including all channels.
+            stream.Write(BitConverter.GetBytes((short)(bytesPerSample)), 0, 2);
+
+            // BitsPerSample    8 bits = 8, 16 bits = 16, etc.
+            stream.Write(BitConverter.GetBytes((short)(bitsPerSample)), 0, 2);
+
+            // Subchunk2ID Contains the letters "data" (0x64617461 big-endian form).
+            stream.Write(encoding.GetBytes("data"), 0, 4);
+
+            // NOTE to be filled in later
+            stream.Write(BitConverter.GetBytes(0), 0, 4);
+        }
+
+        public void UpdateWavHeader(Stream stream)
+        {
+            if (!stream.CanSeek) throw new Exception("Can't seek stream to update wav header");
+
+            var oldPos = stream.Position;
+
+            // ChunkSize  36 + SubChunk2Size
+            stream.Seek(4, SeekOrigin.Begin);
+            stream.Write(BitConverter.GetBytes((int)stream.Length - 8), 0, 4);
+
+            // Subchunk2Size == NumSamples * NumChannels * BitsPerSample/8 This is the number of bytes in the data.
+            stream.Seek(40, SeekOrigin.Begin);
+            stream.Write(BitConverter.GetBytes((int)stream.Length - 44), 0, 4);
+
+            stream.Seek(oldPos, SeekOrigin.Begin);
         }
 
     }
