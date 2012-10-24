efe1fdf5c99b74c103566b57b46d767d53e930f1
Madhur <mg@bsb.in>
2012-10-24 21:30:34 +0530

    Removing unused usings

--- windows-client/View/FreeSMS.xaml.cs
+++ windows-client/View/FreeSMS.xaml.cs
@@ -1,17 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using Microsoft.Phone.Controls;
 using windows_client.utils;
-using Microsoft.Phone.Tasks;
 using System.Diagnostics;
 
 namespace windows_client.View
