5d775896f15e7d2fab1cfe508aada2c7c8d4e979
Madhur <mg@bsb.in>
2012-11-26 21:03:00 +0530

    Copy changes for attachments

--- windows-client/HikeConstants.cs
+++ windows-client/HikeConstants.cs
@@ -55,7 +55,7 @@ namespace windows_client
         public static readonly string FILES_BYTE_LOCATION = FILE_TRANSFER_LOCATION + "/FileBytes";
         public static readonly string FILES_THUMBNAILS = FILE_TRANSFER_LOCATION + "/Thumbnails";
         public static readonly string FILES_ATTACHMENT = FILE_TRANSFER_LOCATION + "/Attachments";
-        public static readonly string FILES_MESSAGE_PREFIX = "I sent you a file. To view go to ";
+        public static readonly string FILES_MESSAGE_PREFIX = "Sent you a %s. To view go to ";
 
         public static readonly string SHARED_FILE_LOCATION = "/shared/transfers";
         public static readonly string FILE_TRANSFER_BASE_URL = AccountUtils.FILE_TRANSFER_BASE + "/user/ft";
