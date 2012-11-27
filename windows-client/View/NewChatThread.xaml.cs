ebc87ebc249e30d81d701f65cc744f0694182a37
Gautam <gk@bsb.in>
2012-11-27 14:35:48 +0530

    Fixed bug#1794 Incorrect avatar image is shown for group chat

--- windows-client/View/NewChatThread.xaml.cs
+++ windows-client/View/NewChatThread.xaml.cs
@@ -574,7 +574,7 @@ namespace windows_client.View
                 processGroupJoin(true);
                 isOnHike = true;
                 isGroupChat = true;
-                userImage.Source = UI_Utils.Instance.DefaultAvatarBitmapImage; //TODO show new group default image
+                userImage.Source = UI_Utils.Instance.DefaultGroupImage;
 
                 /* This is done so that after Tombstone when this page is launched, no group is created again and again */
                 ConversationListObject convObj = new ConversationListObject();
