0c6f53aa5252c07e66b95670ddcb047cd94759ca
Gautam <gk@bsb.in>
2012-12-08 19:23:46 +0530

    Added localization support to welcome, enternumber, pin screen

--- windows-client/Languages/AppResources.Designer.cs
+++ windows-client/Languages/AppResources.Designer.cs
@@ -61,6 +61,177 @@ namespace windows_client.Languages {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Next.
+        /// </summary>
+        public static string AppBar_Next_Btn {
+            get {
+                return ResourceManager.GetString("AppBar_Next_Btn", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Connectivity issue..
+        /// </summary>
+        public static string Connectivity_Issue {
+            get {
+                return ResourceManager.GetString("Connectivity_Issue", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Enter your phone number below to get started :.
+        /// </summary>
+        public static string EnterNumber_EnterNum_TxtBlk {
+            get {
+                return ResourceManager.GetString("EnterNumber_EnterNum_TxtBlk", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Incorrect phone number..
+        /// </summary>
+        public static string EnterNumber_IncorrectPh_TxtBlk {
+            get {
+                return ResourceManager.GetString("EnterNumber_IncorrectPh_TxtBlk", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Incorrect Phone Number.
+        /// </summary>
+        public static string EnterNumber_MsgBoxHeader_Msg {
+            get {
+                return ResourceManager.GetString("EnterNumber_MsgBoxHeader_Msg", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Enter valid phone number..
+        /// </summary>
+        public static string EnterNumber_MsgBoxText_Msg {
+            get {
+                return ResourceManager.GetString("EnterNumber_MsgBoxText_Msg", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Network Error. Try Again!!.
+        /// </summary>
+        public static string EnterNumber_NE_TxtBlk {
+            get {
+                return ResourceManager.GetString("EnterNumber_NE_TxtBlk", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Phone Number.
+        /// </summary>
+        public static string EnterNumber_Ph_Hint_TxtBox {
+            get {
+                return ResourceManager.GetString("EnterNumber_Ph_Hint_TxtBox", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to SELECT COUNTRY.
+        /// </summary>
+        public static string EnterNumber_SelectCountry_TxtBlk {
+            get {
+                return ResourceManager.GetString("EnterNumber_SelectCountry_TxtBlk", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Verifying your number..
+        /// </summary>
+        public static string EnterNumber_VerifyNumberMsg_TxtBlk {
+            get {
+                return ResourceManager.GetString("EnterNumber_VerifyNumberMsg_TxtBlk", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Couldn&apos;t call you. Try again later..
+        /// </summary>
+        public static string EnterPin_CallErrorMsg_MsgBox {
+            get {
+                return ResourceManager.GetString("EnterPin_CallErrorMsg_MsgBox", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Calling you for PIN..
+        /// </summary>
+        public static string EnterPin_CallingMsg_MsgBox {
+            get {
+                return ResourceManager.GetString("EnterPin_CallingMsg_MsgBox", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Call me for PIN.
+        /// </summary>
+        public static string EnterPin_CallMe_Btn {
+            get {
+                return ResourceManager.GetString("EnterPin_CallMe_Btn", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Pin.
+        /// </summary>
+        public static string EnterPin_Pin {
+            get {
+                return ResourceManager.GetString("EnterPin_Pin", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to PIN not valid. Please try again..
+        /// </summary>
+        public static string EnterPin_PinError_TxtBlk {
+            get {
+                return ResourceManager.GetString("EnterPin_PinError_TxtBlk", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Thanks! Did you get an SMS pin? Enter it below :.
+        /// </summary>
+        public static string EnterPin_PinMsg_TxtBlk {
+            get {
+                return ResourceManager.GetString("EnterPin_PinMsg_TxtBlk", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Didn&apos;t receive an SMS?.
+        /// </summary>
+        public static string EnterPin_SMSError_TxtBlk {
+            get {
+                return ResourceManager.GetString("EnterPin_SMSError_TxtBlk", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Tap Here.
+        /// </summary>
+        public static string EnterPin_Taphere_TxtBlk {
+            get {
+                return ResourceManager.GetString("EnterPin_Taphere_TxtBlk", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Entered a wrong number?.
+        /// </summary>
+        public static string EnterPin_WrongNumMsg_TxtBlk {
+            get {
+                return ResourceManager.GetString("EnterPin_WrongNumMsg_TxtBlk", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Tap next to confirm that you are 13+ years of age.
         /// </summary>
         public static string WelcomePage_13YrsMsg_TxtBlk {
@@ -75,15 +246,6 @@ namespace windows_client.Languages {
         public static string WelcomePage_Accept_AppBar {
             get {
                 return ResourceManager.GetString("WelcomePage_Accept_AppBar", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to Connectivity issue..
-        /// </summary>
-        public static string WelcomePage_Connectivity_TxtBlk {
-            get {
-                return ResourceManager.GetString("WelcomePage_Connectivity_TxtBlk", resourceCulture);
             }
         }
         
