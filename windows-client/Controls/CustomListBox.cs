28f59f84be44176df07849f6eaa4da3d12f072fe
Madhur <mg@bsb.in>
2012-12-15 16:45:19 +0530

    Added cutomListBox to fix scrolling issues

---
+++ windows-client/Controls/CustomListBox.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Controls;
+
+namespace windows_client.Controls
+{
+    public class CustomListBox : ListBox
+    {
+        public void ScrollToBottom()
+        {
+            var scrollviewer = GetTemplateChild("ScrollViewer") as ScrollViewer;
+            scrollviewer.ScrollToVerticalOffset(scrollviewer.ScrollableHeight);
+        }
+    }
+}
