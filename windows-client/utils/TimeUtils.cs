e09ed4de66aa83754d4d2fa5d467c18cce5ff9be
Madhur <mg@bsb.in>
2012-12-18 16:05:21 +0530

    Bug# 1827 - Changing precision of timestamp in hikepacket from secs to millisecs

--- windows-client/utils/TimeUtils.cs
+++ windows-client/utils/TimeUtils.cs
@@ -151,5 +151,13 @@ namespace windows_client.utils
             ticks /= 10000000; //Convert windows ticks to seconds
             return ticks;        
         }
+
+        public static long getCurrentTimeTicks()
+        {
+            long ticks = DateTime.UtcNow.Ticks - DateTime.Parse("01/01/1970 00:00:00").Ticks;
+            ticks /= 10000; //Convert windows ticks to seconds
+            return ticks;
+        }
+
     }
 }
