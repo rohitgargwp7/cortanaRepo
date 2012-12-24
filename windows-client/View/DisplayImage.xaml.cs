8455a525ff31d431532635a073631a0c7fddfc73
Madhur <mg@bsb.in>
2012-12-25 00:01:41 +0530

    1) Removed pinch and zoom 2) Changes for showing profile images 3) Removing key from phoneAppService

--- windows-client/View/DisplayImage.xaml.cs
+++ windows-client/View/DisplayImage.xaml.cs
@@ -6,6 +6,8 @@ using System.Windows.Media.Imaging;
 using System.IO;
 using windows_client.DbUtils;
 using System.IO.IsolatedStorage;
+using windows_client.utils;
+using Newtonsoft.Json.Linq;
 namespace windows_client.View
 {
     public partial class DisplayImage : PhoneApplicationPage
@@ -26,6 +28,7 @@ namespace windows_client.View
             if (PhoneApplicationService.Current.State.ContainsKey("objectForFileTransfer"))
             {
                 object[] fileTapped = (object[])PhoneApplicationService.Current.State["objectForFileTransfer"];
+                PhoneApplicationService.Current.State.Remove("objectForFileTransfer");
                 long messsageId = (long)fileTapped[0];
                 string msisdn = (string)fileTapped[1];
                 string filePath = HikeConstants.FILES_BYTE_LOCATION + "/" + msisdn + "/" + Convert.ToString(messsageId);
@@ -38,55 +41,90 @@ namespace windows_client.View
                 fileImage.SetSource(memStream);
                 this.FileImage.Source = fileImage;
             }
-
-        }
-
-        private void OnPinchStarted(object sender, PinchStartedGestureEventArgs e)
-        {
-            initialAngle = transform.Rotation;
-            initialScale = transform.ScaleX;
-        }
-
-        private void OnPinchDelta(object sender, PinchGestureEventArgs e)
-        {
-            //transform.Rotation = initialAngle + e.TotalAngleDelta;
-            transform.ScaleX = initialScale * e.DistanceRatio;
-            transform.ScaleY = initialScale * e.DistanceRatio;
-        }
-
-        private void GestureListener_PinchCompleted(object sender, PinchGestureEventArgs e)
-        {
-            //transform.ScaleX = initialScale;
-            //transform.ScaleY = initialScale;
-        }
-
-        private void GestureListener_DragDelta(object sender, DragDeltaGestureEventArgs e)
-        {
-            // if is not touch enabled or the scale is different than 1 then don’t allow moving
-            if (transform.ScaleX <= 1.1)
-                return;
-            double centerX = transform.CenterX;
-            double centerY = transform.CenterY;
-            double translateX = transform.TranslateX;
-            double translateY = transform.TranslateY;
-            double scale = transform.ScaleX;
-            double width = FileImage.ActualWidth;
-            double height = FileImage.ActualHeight;
-
-            // verify limits to not allow the image to get out of area
-
-            if (centerX - scale * centerX + translateX + e.HorizontalChange < 0 &&
-            centerX + scale * (width - centerX) + translateX + e.HorizontalChange > width)
+            else
             {
-                transform.TranslateX += e.HorizontalChange;
+                object[] profilePicTapped = (object[])PhoneApplicationService.Current.State["displayProfilePic"];
+                PhoneApplicationService.Current.State.Remove("displayProfilePic");
+                string msisdn = (string)profilePicTapped[0];
+                string filePath = msisdn + "_fullView";
+                byte[] fullViewBytes = MiscDBUtil.getThumbNailForMsisdn(filePath);
+                if (fullViewBytes != null && fullViewBytes.Length > 0)
+                {
+                    MemoryStream memStream = new MemoryStream(fullViewBytes);
+                    memStream.Seek(0, SeekOrigin.Begin);
+                    fileImage = new BitmapImage();
+                    fileImage.SetSource(memStream);
+                    this.FileImage.Source = fileImage;
+                }
+                else
+                {
+                    if (!Utils.isGroupConversation(msisdn))
+                    {
+                        AccountUtils.createGetRequest("/account/avatar/" + msisdn + "?fullsize=1", getProfilePic_Callback);
+                    }
+                    else
+                    {
+                        AccountUtils.createGetRequest("/group/" + msisdn + "/avatar/?fullsize=1", getProfilePic_Callback);
+                    }
+                }
             }
-
-            if (centerY - scale * centerY + translateY + e.VerticalChange < 0 &&
-            centerY + scale * (height - centerY) + translateY + e.VerticalChange > height)
-            {
-                transform.TranslateY += e.VerticalChange;
-            }
-            return;
         }
+
+        public void getProfilePic_Callback(byte[] fullBytes)
+        {
+            if (fullBytes != null && fullBytes.Length > 0)
+            {
+                Deployment.Current.Dispatcher.BeginInvoke(() =>
+                {
+                    MemoryStream memStream = new MemoryStream(fullBytes);
+                    memStream.Seek(0, SeekOrigin.Begin);
+                    fileImage = new BitmapImage();
+                    fileImage.SetSource(memStream);
+                    this.FileImage.Source = fileImage;
+                });
+            }
+        }
+        //private void OnPinchStarted(object sender, PinchStartedGestureEventArgs e)
+        //{
+        //    initialAngle = transform.Rotation;
+        //    initialScale = transform.ScaleX;
+        //}
+
+        //private void OnPinchDelta(object sender, PinchGestureEventArgs e)
+        //{
+        //    //transform.Rotation = initialAngle + e.TotalAngleDelta;
+        //    transform.ScaleX = initialScale * e.DistanceRatio;
+        //    transform.ScaleY = initialScale * e.DistanceRatio;
+        //}
+
+
+        //private void GestureListener_DragDelta(object sender, DragDeltaGestureEventArgs e)
+        //{
+        //    // if is not touch enabled or the scale is different than 1 then don’t allow moving
+        //    if (transform.ScaleX <= 1.1)
+        //        return;
+        //    double centerX = transform.CenterX;
+        //    double centerY = transform.CenterY;
+        //    double translateX = transform.TranslateX;
+        //    double translateY = transform.TranslateY;
+        //    double scale = transform.ScaleX;
+        //    double width = FileImage.ActualWidth;
+        //    double height = FileImage.ActualHeight;
+
+        //    // verify limits to not allow the image to get out of area
+
+        //    if (centerX - scale * centerX + translateX + e.HorizontalChange < 0 &&
+        //    centerX + scale * (width - centerX) + translateX + e.HorizontalChange > width)
+        //    {
+        //        transform.TranslateX += e.HorizontalChange;
+        //    }
+
+        //    if (centerY - scale * centerY + translateY + e.VerticalChange < 0 &&
+        //    centerY + scale * (height - centerY) + translateY + e.VerticalChange > height)
+        //    {
+        //        transform.TranslateY += e.VerticalChange;
+        //    }
+        //    return;
+        //}
     }
 }
