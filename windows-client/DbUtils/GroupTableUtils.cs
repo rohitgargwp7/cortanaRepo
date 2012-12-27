6979a6193080de3dc7b8d38f141250f166d4b58e
Gautam <gk@bsb.in>
2012-12-27 15:12:05 +0530

    Added function to make grp alive

--- windows-client/DbUtils/GroupTableUtils.cs
+++ windows-client/DbUtils/GroupTableUtils.cs
@@ -53,6 +53,19 @@ namespace windows_client.DbUtils
             return true;
         }
 
+        public static void SetGroupAlive(string groupId)
+        {
+            using (HikeChatsDb context = new HikeChatsDb(App.MsgsDBConnectionstring))
+            {
+                GroupInfo cObj = DbCompiledQueries.GetGroupInfoForID(context, groupId).FirstOrDefault();
+                if (cObj == null)
+                    return;
+                cObj.GroupAlive = true;
+                MessagesTableUtils.SubmitWithConflictResolve(context);
+            }
+            return;
+        }
+
         public static GroupInfo getGroupInfoForId(string groupId)
         {
             using (HikeChatsDb context = new HikeChatsDb(App.MsgsDBConnectionstring))
