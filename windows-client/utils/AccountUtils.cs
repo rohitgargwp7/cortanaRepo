e27a63653e231631e369a3f9b7cb5650c5b78300
Madhur <mg@bsb.in>
2012-12-29 01:51:30 +0530

    Removing caching in get request

--- windows-client/utils/AccountUtils.cs
+++ windows-client/utils/AccountUtils.cs
@@ -484,6 +484,7 @@ namespace windows_client.utils
             {
                 request = (HttpWebRequest)HttpWebRequest.Create(requestUrl);
             }
+            request.Headers[HttpRequestHeader.IfModifiedSince] = DateTime.UtcNow.ToString();
             request.BeginGetResponse(GetRequestCallback, new object[] { request, callback });
         }
 
