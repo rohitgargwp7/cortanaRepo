1cad6fa66ae096150881729cb084b17cde309f1b
Madhur <mg@bsb.in>
2013-01-19 01:22:10 +0530

    Image status updates

--- windows-client/Controls/StatusUpdate/ImageStatusUpdate.xaml.cs
+++ windows-client/Controls/StatusUpdate/ImageStatusUpdate.xaml.cs
@@ -1,15 +1,18 @@
-﻿using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Documents;
-using windows_client.utils;
-using System.Windows.Media;
-using System.Text.RegularExpressions;
-using System;
-using Microsoft.Phone.Tasks;
+﻿using windows_client.utils;
+using System.Windows.Media.Imaging;
+using windows_client.Languages;
 
 namespace windows_client.Controls.StatusUpdate
 {
     public partial class ImageStatusUpdate : StatusUpdateBox
     {
+        public ImageStatusUpdate(string userName, BitmapImage userImage, BitmapImage statusImageBitmap, string locationName, long timestamp)
+            : base(userName, userImage, StatusType.IMAGE_UPDATE)
+        {
+            InitializeComponent();
+            this.statusTextTxtBlk.Text = string.Format(AppResources.StatusUpdate_YouCanNowSeeUpdates_TxtBlk, locationName);
+            this.timestampTxtBlk.Text = TimeUtils.getRelativeTime(timestamp);
+            this.statusImage.Source = statusImageBitmap;
+        }
     }
 }
