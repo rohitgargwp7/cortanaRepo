68844a214bddc066f9d148fa73f7c5e1c25042e7
Madhur <mg@bsb.in>
2013-01-18 12:31:39 +0530

    Adding placeholder files for status update custom controls

---
+++ windows-client/Controls/StatusUpdate/ImageStatusUpdate.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using windows_client.utils;
+using System.Windows.Media;
+using System.Text.RegularExpressions;
+using System;
+using Microsoft.Phone.Tasks;
+
+namespace windows_client.Controls.StatusUpdate
+{
+    public partial class ImageStatusUpdate : StatusUpdateBox
+    {
+    }
+}
---
+++ windows-client/Controls/StatusUpdate/StatusUpdateBox.cs
@@ -0,0 +1,8 @@
+﻿using System.Windows.Controls;
+
+namespace windows_client.Controls.StatusUpdate
+{
+    public class StatusUpdateBox : UserControl
+    {
+    }
+}
