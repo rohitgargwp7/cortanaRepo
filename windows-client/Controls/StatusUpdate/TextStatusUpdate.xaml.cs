0427624448e66b90b0351198ea63d5adb9c08368
Madhur <mg@bsb.in>
2013-01-19 00:50:21 +0530

    Text status update UI

--- windows-client/Controls/StatusUpdate/TextStatusUpdate.xaml.cs
+++ windows-client/Controls/StatusUpdate/TextStatusUpdate.xaml.cs
@@ -6,10 +6,19 @@ using System.Windows.Media;
 using System.Text.RegularExpressions;
 using System;
 using Microsoft.Phone.Tasks;
+using System.Windows.Media.Imaging;
+using windows_client.Languages;
 
 namespace windows_client.Controls.StatusUpdate
 {
     public partial class TextStatusUpdate : StatusUpdateBox
     {
+        public TextStatusUpdate(string userName, BitmapImage userImage, string locationName, long timestamp)
+            : base(userName, userImage)
+        {
+            InitializeComponent();
+            this.statusTextTxtBlk.Text = string.Format(AppResources.StatusUpdate_YouCanNowSeeUpdates_TxtBlk, locationName);
+            this.timestampTxtBlk.Text = TimeUtils.getRelativeTime(timestamp);
+        }
     }
 }
