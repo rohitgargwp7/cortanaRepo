b46dcb5b87f8a59fcc8f2c7b3f07180d1bf049a2
Madhur <mg@bsb.in>
2013-01-18 19:06:24 +0530

    Base class and custom control for friend request

--- windows-client/Controls/StatusUpdate/FriendRequestStatus.xaml.cs
+++ windows-client/Controls/StatusUpdate/FriendRequestStatus.xaml.cs
@@ -6,10 +6,122 @@ using System.Windows.Media;
 using System.Text.RegularExpressions;
 using System;
 using Microsoft.Phone.Tasks;
+using System.Windows.Media.Imaging;
+using windows_client.Languages;
+using Microsoft.Phone.Controls;
+using System.ComponentModel;
 
 namespace windows_client.Controls.StatusUpdate
 {
-    public partial class FriendRequestStatus : StatusUpdateBox
+    public partial class FriendRequestStatus : StatusUpdateBox, INotifyPropertyChanged, INotifyPropertyChanging
     {
+
+        private EventHandler<GestureEventArgs> _yesTap;
+        private EventHandler<GestureEventArgs> _noTap;
+        private string _seeUpdates;
+
+        public EventHandler<GestureEventArgs> YesTap
+        {
+            get
+            {
+                return _yesTap;
+            }
+            set
+            {
+                if (value != _yesTap)
+                {
+                    NotifyPropertyChanging("YesTap");
+                    _yesTap = value;
+                    NotifyPropertyChanged("YesTap");
+                }
+            }
+        }
+
+        public EventHandler<GestureEventArgs> NoTap
+        {
+            get
+            {
+                return _noTap;
+            }
+            set
+            {
+                if (value != _noTap)
+                {
+                    NotifyPropertyChanging("NoTap");
+                    _noTap = value;
+                    NotifyPropertyChanged("NoTap");
+                }
+            }
+        }
+
+        public string SeeUpdates
+        {
+            get
+            {
+                return _seeUpdates;
+            }
+            set
+            {
+                if (value != _seeUpdates)
+                {
+                    NotifyPropertyChanging("SeeUpdates");
+                    _seeUpdates = value;
+                    NotifyPropertyChanged("SeeUpdates");
+                }
+            }
+        }
+
+        public FriendRequestStatus(string userName, BitmapImage userImage)
+            : base(userName, userImage)
+        {
+            this.SeeUpdates = string.Format(AppResources.StatusUpdate_YouCanNowSeeUpdates_TxtBlk, userName);
+        }
+
+        public FriendRequestStatus(string userName, BitmapImage userImage, 
+            EventHandler<GestureEventArgs> yesTap, EventHandler<GestureEventArgs> noTap)
+            : base(userName, userImage)
+        {
+            this.SeeUpdates = string.Format(AppResources.StatusUpdate_YouCanNowSeeUpdates_TxtBlk, userName);
+            this.YesTap = yesTap;
+            this.NoTap = noTap;
+        }
+
+
+        #region INotifyPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        // Used to notify that a property changed
+        private void NotifyPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                Deployment.Current.Dispatcher.BeginInvoke(() =>
+                {
+                    try
+                    {
+                        PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+                    }
+                    catch { }
+                });
+            }
+        }
+
+        #endregion
+
+        #region INotifyPropertyChanging Members
+
+        public event PropertyChangingEventHandler PropertyChanging;
+
+        // Used to notify that a property is about to change
+        private void NotifyPropertyChanging(string propertyName)
+        {
+            if (PropertyChanging != null)
+            {
+                PropertyChanging(this, new PropertyChangingEventArgs(propertyName));
+            }
+        }
+        #endregion
+
     }
 }
--- windows-client/Controls/StatusUpdate/StatusUpdateBox.cs
+++ windows-client/Controls/StatusUpdate/StatusUpdateBox.cs
@@ -1,8 +1,52 @@
 ﻿using System.Windows.Controls;
+using System.Windows.Media.Imaging;
 
 namespace windows_client.Controls.StatusUpdate
 {
     public class StatusUpdateBox : UserControl
     {
+        private string _userName;
+        private BitmapImage _userImage;
+
+        public string UserName
+        {
+            get 
+            {
+                return _userName;
+            }
+            set 
+            {
+                if (value != _userName)
+                {
+                    _userName = value;
+                }
+            }
+        }
+
+        public BitmapImage UserImage
+        {
+            get
+            {
+                return _userImage;
+            }
+            set
+            {
+                if (value != _userImage)
+                {
+                    _userImage = value;
+                }
+            }
+        }
+
+        public StatusUpdateBox(string userName, BitmapImage userImage)
+        {
+            this.UserName = userName;
+            this.UserImage = userImage;
+        }
+
+        public StatusUpdateBox()
+        {
+        }
+
     }
 }
