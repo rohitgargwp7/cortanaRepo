3069178b2fb40d92d6ddca73ab129f2b39b3d610
Madhur <mg@bsb.in>
2013-01-19 00:45:09 +0530

    LocationStatusUpdate custom control

--- windows-client/Controls/StatusUpdate/LocationStatusUpdate.xaml.cs
+++ windows-client/Controls/StatusUpdate/LocationStatusUpdate.xaml.cs
@@ -1,15 +1,17 @@
-﻿using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Documents;
-using windows_client.utils;
-using System.Windows.Media;
-using System.Text.RegularExpressions;
-using System;
-using Microsoft.Phone.Tasks;
+﻿using windows_client.utils;
+using System.Windows.Media.Imaging;
+using windows_client.Languages;
 
 namespace windows_client.Controls.StatusUpdate
 {
     public partial class LocationStatusUpdate : StatusUpdateBox
     {
+        public LocationStatusUpdate(string userName, BitmapImage userImage, string locationName, long timestamp)
+            : base(userName, userImage)
+        {
+            InitializeComponent();
+            this.locationTxtBlk .Text = string.Format(AppResources.StatusUpdate_YouCanNowSeeUpdates_TxtBlk, locationName);
+            this.timestampTxtBlk.Text = TimeUtils.getRelativeTime(timestamp);
+        }
     }
 }
