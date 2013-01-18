c86c697762e96dcbb23a959a04ae2bf087e6d6c7
Madhur <mg@bsb.in>
2013-01-19 01:19:48 +0530

    Merginf Text and Location updates into one

---
+++ windows-client/Controls/StatusUpdate/LocationOrText.xaml.cs
@@ -0,0 +1,24 @@
+﻿using windows_client.utils;
+using System.Windows.Media.Imaging;
+using windows_client.Languages;
+
+namespace windows_client.Controls.StatusUpdate
+{
+    public partial class LocationOrText : StatusUpdateBox
+    {
+        public LocationOrText(string userName, BitmapImage userImage, StatusType updateType, string locationName, long timestamp)
+            : base(userName, userImage, updateType)
+        {
+            InitializeComponent();
+            this.timestampTxtBlk.Text = TimeUtils.getRelativeTime(timestamp);
+            if (updateType == StatusType.TEXT_UPDATE)
+            {
+                this.statusTextTxtBlk.Text = string.Format(AppResources.StatusUpdate_YouCanNowSeeUpdates_TxtBlk, locationName);
+            }
+            else
+            {
+                this.statusTextTxtBlk.Text = string.Format(AppResources.StatusUpdate_YouCanNowSeeUpdates_TxtBlk, locationName);
+            }
+        }
+    }
+}
