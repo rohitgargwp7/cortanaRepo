e5c373c59965a62f19b7959543a91703a079d17b
Madhur <mg@bsb.in>
2013-01-19 00:35:00 +0530

    Removing property binding & INotify interface

--- windows-client/Controls/StatusUpdate/FriendRequestStatus.xaml.cs
+++ windows-client/Controls/StatusUpdate/FriendRequestStatus.xaml.cs
@@ -13,115 +13,26 @@ using System.ComponentModel;
 
 namespace windows_client.Controls.StatusUpdate
 {
-    public partial class FriendRequestStatus : StatusUpdateBox, INotifyPropertyChanged, INotifyPropertyChanging
+    public partial class FriendRequestStatus : StatusUpdateBox
     {
-
-        private EventHandler<GestureEventArgs> _yesTap;
-        private EventHandler<GestureEventArgs> _noTap;
-        private string _seeUpdates;
-
-        public EventHandler<GestureEventArgs> YesTap
-        {
-            get
-            {
-                return _yesTap;
-            }
-            set
-            {
-                if (value != _yesTap)
-                {
-                    NotifyPropertyChanging("YesTap");
-                    _yesTap = value;
-                    NotifyPropertyChanged("YesTap");
-                }
-            }
-        }
-
-        public EventHandler<GestureEventArgs> NoTap
-        {
-            get
-            {
-                return _noTap;
-            }
-            set
-            {
-                if (value != _noTap)
-                {
-                    NotifyPropertyChanging("NoTap");
-                    _noTap = value;
-                    NotifyPropertyChanged("NoTap");
-                }
-            }
-        }
-
-        public string SeeUpdates
-        {
-            get
-            {
-                return _seeUpdates;
-            }
-            set
-            {
-                if (value != _seeUpdates)
-                {
-                    NotifyPropertyChanging("SeeUpdates");
-                    _seeUpdates = value;
-                    NotifyPropertyChanged("SeeUpdates");
-                }
-            }
-        }
-
-        public FriendRequestStatus(string userName, BitmapImage userImage)
-            : base(userName, userImage)
-        {
-            this.SeeUpdates = string.Format(AppResources.StatusUpdate_YouCanNowSeeUpdates_TxtBlk, userName);
-        }
+        //public FriendRequestStatus(string userName, BitmapImage userImage)
+        //    : base(userName, userImage)
+        //{
+        //    InitializeComponent();
+        //    this.seeUpdatesTxtBlk.Text = string.Format(AppResources.StatusUpdate_YouCanNowSeeUpdates_TxtBlk, userName);
+        //}
 
         public FriendRequestStatus(string userName, BitmapImage userImage, 
             EventHandler<GestureEventArgs> yesTap, EventHandler<GestureEventArgs> noTap)
             : base(userName, userImage)
         {
-            this.SeeUpdates = string.Format(AppResources.StatusUpdate_YouCanNowSeeUpdates_TxtBlk, userName);
-            this.YesTap = yesTap;
-            this.NoTap = noTap;
+            InitializeComponent();
+            this.seeUpdatesTxtBlk.Text = string.Format(AppResources.StatusUpdate_YouCanNowSeeUpdates_TxtBlk, userName);
+
+            var glYes = GestureService.GetGestureListener(this.yesBtn);
+            glYes.Tap += yesTap;
+            var glNo = GestureService.GetGestureListener(this.noBtn);
+            glNo.Tap += noTap;
         }
-
-
-        #region INotifyPropertyChanged Members
-
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        // Used to notify that a property changed
-        private void NotifyPropertyChanged(string propertyName)
-        {
-            if (PropertyChanged != null)
-            {
-                Deployment.Current.Dispatcher.BeginInvoke(() =>
-                {
-                    try
-                    {
-                        PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
-                    }
-                    catch { }
-                });
-            }
-        }
-
-        #endregion
-
-        #region INotifyPropertyChanging Members
-
-        public event PropertyChangingEventHandler PropertyChanging;
-
-        // Used to notify that a property is about to change
-        private void NotifyPropertyChanging(string propertyName)
-        {
-            if (PropertyChanging != null)
-            {
-                PropertyChanging(this, new PropertyChangingEventArgs(propertyName));
-            }
-        }
-        #endregion
-
     }
 }
