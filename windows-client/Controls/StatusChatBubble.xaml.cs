b336c3477852495bf3618a65508809c32e1b7a45
Madhur <mg@bsb.in>
2013-01-28 08:08:03 +0530

    Added chat bubble for status updates

---
+++ windows-client/Controls/StatusChatBubble.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows.Media;
+using windows_client.Model;
+using windows_client.utils;
+
+namespace windows_client.Controls
+{
+    public partial class StatusChatBubble : MyChatBubble 
+    {
+        public StatusChatBubble(ConvMessage cm)
+        {
+            this.statusMessageTxtBlk.Text = this.Text = cm.Message;
+            this.statusTimestampTxtBlk.Text = this.TimeStamp = TimeUtils.getRelativeTime(cm.Timestamp);
+        }
+    }
+}
