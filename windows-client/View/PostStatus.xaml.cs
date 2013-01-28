3c4eb08aef5a0867b1c23249dcfd715b9720be86
rohitwp7 <rohit@bsb.in>
2013-01-28 18:19:24 +0530

    added ui elemets in post status

--- windows-client/View/PostStatus.xaml.cs
+++ windows-client/View/PostStatus.xaml.cs
@@ -7,14 +7,35 @@ using System.Windows.Controls;
 using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
+using windows_client.Languages;
 
 namespace windows_client.View
 {
     public partial class PostStatus : PhoneApplicationPage
     {
+        private ApplicationBar appBar;
         public PostStatus()
         {
             InitializeComponent();
+            txtStatus.Focus();
+            appBar = new ApplicationBar();
+            appBar.Mode = ApplicationBarMode.Default;
+            appBar.Opacity = 1;
+            appBar.IsVisible = true;
+            appBar.IsMenuEnabled = false;
+
+            ApplicationBarIconButton postStatusIcon = new ApplicationBarIconButton();
+            postStatusIcon.IconUri = new Uri("/View/images/icon_status.png", UriKind.Relative);
+            postStatusIcon.Text = AppResources.AppBar_Next_Btn;
+            postStatusIcon.Click += new EventHandler(btnPostStatus_Click);
+            postStatusIcon.IsEnabled = false;
+            appBar.Buttons.Add(postStatusIcon);
+            postStatusPage.ApplicationBar = appBar;
         }
+
+        private void btnPostStatus_Click(object sender, EventArgs e)
+        {
+        }
+
     }
 }
