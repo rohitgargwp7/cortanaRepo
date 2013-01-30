70bf69c780d53375fa8df3ab2f69703e09526095
Gautam Kanwar <gk@bsb.in>
2013-01-30 18:57:57 +0530

    Making these objects from convlistobj only

--- windows-client/Controls/StatusUpdate/FriendRequestStatus.xaml.cs
+++ windows-client/Controls/StatusUpdate/FriendRequestStatus.xaml.cs
@@ -2,11 +2,14 @@
 using System.Windows.Media.Imaging;
 using windows_client.Languages;
 using Microsoft.Phone.Controls;
+using windows_client.Model;
 
 namespace windows_client.Controls.StatusUpdate
 {
     public partial class FriendRequestStatus : StatusUpdateBox
     {
+        private ConversationListObject convObj;
+
         public FriendRequestStatus(string userName, BitmapImage userImage, string msisdn,
             EventHandler<GestureEventArgs> yesTap, EventHandler<GestureEventArgs> noTap)
             : base(userName, userImage, msisdn)
@@ -23,6 +26,30 @@ namespace windows_client.Controls.StatusUpdate
                 var glNo = GestureService.GetGestureListener(this.noBtn);
                 glNo.Tap += noTap;
             }
+        }
+
+        public FriendRequestStatus(ConversationListObject c, EventHandler<GestureEventArgs> yesTap, EventHandler<GestureEventArgs> noTap)
+            : base(c)
+        {
+            InitializeComponent();
+            convObj = c;
+            this.seeUpdatesTxtBlk.Text = string.Format(AppResources.StatusUpdate_YouCanNowSeeUpdates_TxtBlk, c.NameToShow);
+            if (yesTap != null)
+            {
+                var glYes = GestureService.GetGestureListener(this.yesBtn);
+                glYes.Tap += yesTap;
+            }
+            if (noTap != null)
+            {
+                var glNo = GestureService.GetGestureListener(this.noBtn);
+                glNo.Tap += noTap;
+            }
+        }
+
+        public void update(ConversationListObject c)
+        {
+            this.seeUpdatesTxtBlk.Text = string.Format(AppResources.StatusUpdate_YouCanNowSeeUpdates_TxtBlk, c.NameToShow);
+            this.UserImage = c.AvatarImage;
         }
     }
 }
