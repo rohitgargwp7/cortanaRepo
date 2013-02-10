7f78b3ae910bc5229f5a951876678d838838cf3d
rohitwp7 <rohit@bsb.in>
2013-02-10 12:28:41 +0530

    fixed error of type contact

--- windows-client/DbUtils/DbConversationListener.cs
+++ windows-client/DbUtils/DbConversationListener.cs
@@ -97,7 +97,7 @@ namespace windows_client.DbUtils
                     convMessage.Message = String.Format(AppResources.FILES_MESSAGE_PREFIX, AppResources.Video_Txt) + HikeConstants.FILE_TRANSFER_BASE_URL +
                         "/" + fileKey;
                 }
-                else if (contentType.Contains(HikeConstants.VIDEO))
+                else if (contentType.Contains(HikeConstants.CONTACT))
                 {
                     convMessage.Message = String.Format(AppResources.FILES_MESSAGE_PREFIX, "Contact") + HikeConstants.FILE_TRANSFER_BASE_URL +
                         "/" + fileKey;
