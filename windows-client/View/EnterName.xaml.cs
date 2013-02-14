3a03087e05be5666ea44c005867d9fd5b60d7c6b
rohitwp7 <rohit@bsb.in>
2013-02-14 17:56:34 +0530

    moved Nux logic to Contact Bool Scanning Completed

--- windows-client/View/EnterName.xaml.cs
+++ windows-client/View/EnterName.xaml.cs
@@ -132,14 +132,16 @@ namespace windows_client
                 App.WriteToIsoStorageSettings(App.SHOW_FREE_SMS_SETTING, false);
             }
 
-            List<ContactInfo> listContactInfo;
+            List<ContactInfo> listFamilyMembers = new List<ContactInfo>();
+            List<ContactInfo> listCloseFriends = new List<ContactInfo>();
+            ProcessNuxContacts(listFamilyMembers, listCloseFriends);
             SmileyParser.Instance.initializeSmileyParser();
-            if (App.appSettings.TryGetValue(HikeConstants.CLOSE_FRIENDS_NUX, out listContactInfo) && listContactInfo.Count > 1)
+            if (listCloseFriends.Count > 1)
             {
                 App.WriteToIsoStorageSettings(App.PAGE_STATE, App.PageState.NUX_SCREEN);
                 nextPage = new Uri("/View/NUX_InviteFriends.xaml", UriKind.Relative);
             }
-            else if(App.appSettings.TryGetValue(HikeConstants.FAMILY_MEMBERS_NUX, out listContactInfo) && listContactInfo.Count > 1)
+            else if (listFamilyMembers.Count > 1)
             {
                 App.WriteToIsoStorageSettings(App.PAGE_STATE, App.PageState.NUX_SCREEN);
                 nextPage = new Uri("/View/NUX_InviteFriends.xaml", UriKind.Relative);
@@ -166,6 +168,110 @@ namespace windows_client
             }
         }
 
+        public void ProcessNuxContacts(List<ContactInfo> listFamilyMembers, List<ContactInfo> listCloseFriends)
+        {
+            if (AccountUtils.server_contacts != null && AccountUtils.server_contacts.Count > 0 && listFamilyMembers != null && listCloseFriends != null)
+            {
+                string lastName = GetLastName();
+                bool isLastNameCheckApplicable = lastName != null;
+                AccountUtils.server_contacts.Sort();
+                foreach (ContactInfo cn in AccountUtils.server_contacts)
+                {
+                    if (!cn.OnHike)
+                    {
+                        bool markedForNux = false;
+                        if (listFamilyMembers.Count < 31)
+                        {
+                            if (isLastNameCheckApplicable)
+                            {
+                                if (!string.IsNullOrEmpty(cn.Name))
+                                {
+                                    string[] nameArray = cn.Name.Trim().Split(' ');
+                                    if (nameArray.Length > 1)
+                                    {
+                                        string curlastName = nameArray[nameArray.Length - 1].ToLower();
+                                        if (curlastName.Trim().ToLower() == lastName)
+                                        {
+                                            listFamilyMembers.Add(cn);
+                                            markedForNux = true;
+                                        }
+                                    }
+                                }
+                            }
+                            if (!markedForNux && MatchFromFamilyVocab(cn.Name))
+                            {
+                                markedForNux = true;
+                                listFamilyMembers.Add(cn);
+                            }
+                        }
+
+                        if (!markedForNux && cn.NuxMatchScore > 0)
+                        {
+                            markedForNux = true;
+                            listCloseFriends.Add(cn);
+                        }
+                    }
+                }
+
+                if (listCloseFriends.Count < 31)
+                {
+                    AccountUtils.server_contacts.Sort();//normal sorting required as rest has nothing related to nux
+                    int contactAdded = 0;
+                    int countRequired = 30 - listCloseFriends.Count;
+                    foreach (ContactInfo contact in AccountUtils.server_contacts)
+                    {
+                        if (contactAdded == countRequired)
+                            break;
+                        if (!contact.OnHike && !listCloseFriends.Contains(contact) && !listFamilyMembers.Contains(contact))
+                        {
+                            listCloseFriends.Add(contact);
+                            contactAdded++;
+                        }
+                    }
+                }
+                else
+                {
+                    listCloseFriends.Sort(new ContactCompare());
+                    listCloseFriends.RemoveRange(30, listCloseFriends.Count - 30);
+                }
+                App.WriteToIsoStorageSettings(HikeConstants.CLOSE_FRIENDS_NUX, listCloseFriends);
+
+                if (listFamilyMembers.Count > 0)
+                    App.WriteToIsoStorageSettings(HikeConstants.FAMILY_MEMBERS_NUX, listFamilyMembers);
+            }
+        }
+        public static string GetLastName()
+        {
+            string name;
+            App.appSettings.TryGetValue(App.ACCOUNT_NAME, out name);
+            if (name == null)
+                return null;
+
+            string[] nameArray = name.Trim().Split(' ');
+            if (nameArray.Length == 1)
+                return null;
+
+            return nameArray[nameArray.Length - 1].ToLower();
+        }
+
+        #region FAMILY VOCABULARY
+
+        private static string[] familyVocab = new string[] { "aunt", "aunty", "auntie", "uncle", "grandma", "granny", "grandpa", "nanna", "cousin", "‘opà", "aayi", "abatyse", "abba", "abba", "abbi", "aboji", "abonim", "ahm", "äiti", "ama", "amai", "amca", "amma", "ammee", "ammi", "ana", "anne", "anneanne", "anya", "apa", "appa", "apu", "athair", "atta", "aunt", "auntie", "aunty", "ayah", "baabaa", "baba", "babba", "babbo", "banketi", "bapa", "bata", " dai", "bebe", "beta", "beti", "bhabhi", "bhai", "bhaiya", "biang", "bro", "buwa", "chacha", "chachu", "dad", "dada", "daddy", "dadi", "daidí", "daya", "dayı", "dede", "didi", "eadni", "édesapa", "eje", "ema", "emä", "emak", "emo", "ewe", "far", "father", "foter", "fu", "grandma", "grandpa", "haakoro", "haakui", "haha", "ibu", "iloy", "inahan", "induk", "isa", "isä", "itay", "janak", "kantaäiti", "kardeş-im", "kızım", "kohake", "kuzen", "ma", "maa", "macii", "madar", "madèr", "màder", "madr", "mädra", "madre", "mãe", "mai", "maica", "maire", "maji", "majka", "makuahine", "mam", "mama", "mamá", "maman", "mami", "mamm'", "mamm", "mamma", "mamu", "mána", "màna", "mare", "mari", "mat'", "mataji", "mater", "máthair", "mati", "máti", "matka", "matre, mamma", "matri", "me", "mèder", "medra", "mëmë", "mére", "mère", "moæ", "moder", "móðir", "moeder", "moer", "mojer", "mom", "mommy", "mor", "morsa", "mother", "motina", "mueter", "mum", "mummy", "mumsy", "muter", "mutter", "mutti", "mytyr", "mzaa", "mzazi", "nai", "nana", "nanay", "nani", "nay", "nënë", "ñuke", "ñuque", "nyokap", "ôèe", "oğlum", "ojciec", "okaasan", "omm", "oppa", "otac", "otec", "otosan", "pabo", "pai", "pak", "panjo", "papa", "papá", "papà", "papi", "pappa", "pappie", "pare", "parinte", "pater", "patri", "patrino", "pedar", "pita-ji", "pitaji", "pitar", "pop", "popà", "poppa", "pops", "pradininkas", "protevis", "pupà", "reny", "salentino", "sis", "tad", "taica", "tata", "táta", "tàtah", "tatay", "tateh", "tatti", "tay", "tevas", "tevs", "teyze", "uma", "uncle", "vader", "valide", "vieja", "viejo", "yebba", "yeğen", "yenge", "badima", "memaw", "meemaw", "妈", "妈妈", "老妈", "老公", "宝贝", "老婆", "宝贝", "爸", "爸爸", "老爸", "女儿", "闺女", "儿子", "哥", "哥哥", "弟", "弟弟", "姐", "姐姐", "妹", "妹妹", "祖母", "奶奶", "大姨", "小姨", "姑姑", "舅舅", "大舅", "小舅", "叔叔", "伯伯", "表姐", "表妹", "表哥", "表弟", "侄子", "侄女", "uncle", "mama", "mamu", "chacha", "chachu", "mom", "dad", "bhai", "bhaiya", "didi" };
+
+        #endregion
+
+        public static bool MatchFromFamilyVocab(string completeName)
+        {
+            if (string.IsNullOrEmpty(completeName))
+                return false;
+
+            foreach (string vocabKey in familyVocab)
+            {
+                if (completeName.ToLower().Contains(vocabKey))
+                    return true;
+            }
+            return false;
+        }
         protected override void OnNavigatedTo(System.Windows.Navigation.NavigationEventArgs e)
         {
             base.OnNavigatedTo(e);
--- windows-client/utils/AccountUtils.cs
+++ windows-client/utils/AccountUtils.cs
@@ -35,6 +35,7 @@ namespace windows_client.utils
 
         private static readonly int STAGING_PORT = 8080;
 
+        public static List<ContactInfo> server_contacts;
         public static bool IsProd
         {
             get
@@ -785,8 +786,6 @@ namespace windows_client.utils
         {
             try
             {
-                List<ContactInfo> listFamilyMembers = new List<ContactInfo>();
-                List<ContactInfo> listCloseFriends = new List<ContactInfo>();
                 if ((obj == null) || HikeConstants.FAIL == (string)obj[HikeConstants.STAT])
                 {
                     return null;
@@ -811,19 +810,12 @@ namespace windows_client.utils
                     GroupManager.Instance.LoadGroupCache();
                 }
 
-                List<ContactInfo> server_contacts = new List<ContactInfo>();
+                server_contacts = new List<ContactInfo>();
                 IEnumerator<KeyValuePair<string, JToken>> keyVals = addressbook.GetEnumerator();
                 KeyValuePair<string, JToken> kv;
                 int count = 0;
                 int totalContacts = 0;
-                string lastName = GetLastName();
-                bool isLastNameCheckApplicable = lastName != null;
-                bool nuxRequired = false;
-                App.PageState ps;
-                if (App.appSettings.TryGetValue(App.PAGE_STATE, out ps))
-                {
-                    nuxRequired = ps != App.PageState.CONVLIST_SCREEN;
-                }
+
                 while (keyVals.MoveNext())
                 {
                     kv = keyVals.Current;
@@ -839,56 +831,12 @@ namespace windows_client.utils
                             continue;
                         }
                         bool onhike = (bool)entry["onhike"];
-                        ContactInfo cn = new ContactInfo(kv.Key, msisdn, cList[i].Name, onhike, cList[i].PhoneNo);
+                        ContactInfo cinfo = cList[i];
+                        ContactInfo cn = new ContactInfo(kv.Key, msisdn, cinfo.Name, onhike, cinfo.PhoneNo);
 
-                        #region NUX SCANNING
-                        if (nuxRequired)
-                        {
-
-                            if (!cn.OnHike)
-
-                            {
-                                bool markedForNux = false;
-                                if (listFamilyMembers.Count < 31)
-                                {
-                                    if (isLastNameCheckApplicable)
-                                    {
-
-                                        if (!string.IsNullOrEmpty(cn.Name))
-                                        {
-                                            string[] nameArray = cn.Name.Trim().Split(' ');
-                                            if (nameArray.Length > 1)
-                                            {
-                                                string curlastName = nameArray[nameArray.Length - 1].ToLower();
-                                                if (curlastName.Trim().ToLower() == lastName)
-                                                {
-                                                    listFamilyMembers.Add(cn);
-                                                    markedForNux = true;
-                                                }
-                                            }
-                                        }
-                                    }
-                                    if (!markedForNux && MatchFromFamilyVocab(cn.Name))
-                                    {
-                                        listFamilyMembers.Add(cn);
-                                    }
-                                }
-
-                                if (!markedForNux && cList[i].NuxMatchScore > 0)
-                                {
-                                    markedForNux = true;
-                                    listCloseFriends.Add(cn);
-                                }
-                                if (markedForNux)
-                                {
-                                    cn.Avatar = cList[i].Avatar;
-                                    cn.HasPicture = cList[i].HasPicture;
-                                    cn.NuxMatchScore = cList[i].NuxMatchScore;
-                                }
-                            }
-
-                        }
-                        #endregion
+                        cn.HasPicture = cinfo.HasPicture;
+                        cn.NuxMatchScore = cinfo.NuxMatchScore;
+                        cn.Avatar = cinfo.Avatar;
 
                         if (!isRefresh) // this is case for new installation
                         {
@@ -946,35 +894,6 @@ namespace windows_client.utils
                         totalContacts++;
                     }
                 }
-                if (nuxRequired)
-                {
-                    if (listCloseFriends.Count < 31)
-                    {
-                        server_contacts.Sort();
-                        int contactAdded = 0;
-                        int countRequired = 30 - listCloseFriends.Count;
-                        foreach (ContactInfo contact in server_contacts)
-                        {
-                            if (contactAdded == countRequired)
-                                break;
-                            if (!contact.OnHike && !listCloseFriends.Contains(contact) && !listFamilyMembers.Contains(contact))
-                            {
-                                listCloseFriends.Add(contact);
-                                contactAdded++;
-                            }
-                        }
-                    }
-                    else
-                    {
-                        listCloseFriends.Sort(new ContactCompare());
-                        listCloseFriends.RemoveRange(30, listCloseFriends.Count - 30);
-                    }
-                    App.WriteToIsoStorageSettings(HikeConstants.CLOSE_FRIENDS_NUX, listCloseFriends);
-
-                    if (listFamilyMembers.Count > 0)
-                        App.WriteToIsoStorageSettings(HikeConstants.FAMILY_MEMBERS_NUX, listFamilyMembers);
-                }
-
                 if (isFavSaved)
                     MiscDBUtil.SaveFavourites();
                 if (isPendingSaved)
@@ -996,38 +915,7 @@ namespace windows_client.utils
             }
         }
 
-        public static string GetLastName()
-        {
-            string name;
-            App.appSettings.TryGetValue(App.ACCOUNT_NAME, out name);
-            if (name == null)
-                return null;
 
-            string[] nameArray = name.Trim().Split(' ');
-            if (nameArray.Length == 1)
-                return null;
-
-            return nameArray[nameArray.Length - 1].ToLower();
-        }
-
-        #region FAMILY VOCABULARY
-
-        private static string[] familyVocab = new string[] { "aunt", "aunty", "auntie", "uncle", "grandma", "granny", "grandpa", "nanna", "cousin", "‘opà", "aayi", "abatyse", "abba", "abba", "abbi", "aboji", "abonim", "ahm", "äiti", "ama", "amai", "amca", "amma", "ammee", "ammi", "ana", "anne", "anneanne", "anya", "apa", "appa", "apu", "athair", "atta", "aunt", "auntie", "aunty", "ayah", "baabaa", "baba", "babba", "babbo", "banketi", "bapa", "bata", " dai", "bebe", "beta", "beti", "bhabhi", "bhai", "bhaiya", "biang", "bro", "buwa", "chacha", "chachu", "dad", "dada", "daddy", "dadi", "daidí", "daya", "dayı", "dede", "didi", "eadni", "édesapa", "eje", "ema", "emä", "emak", "emo", "ewe", "far", "father", "foter", "fu", "grandma", "grandpa", "haakoro", "haakui", "haha", "ibu", "iloy", "inahan", "induk", "isa", "isä", "itay", "janak", "kantaäiti", "kardeş-im", "kızım", "kohake", "kuzen", "ma", "maa", "macii", "madar", "madèr", "màder", "madr", "mädra", "madre", "mãe", "mai", "maica", "maire", "maji", "majka", "makuahine", "mam", "mama", "mamá", "maman", "mami", "mamm'", "mamm", "mamma", "mamu", "mána", "màna", "mare", "mari", "mat'", "mataji", "mater", "máthair", "mati", "máti", "matka", "matre, mamma", "matri", "me", "mèder", "medra", "mëmë", "mére", "mère", "moæ", "moder", "móðir", "moeder", "moer", "mojer", "mom", "mommy", "mor", "morsa", "mother", "motina", "mueter", "mum", "mummy", "mumsy", "muter", "mutter", "mutti", "mytyr", "mzaa", "mzazi", "nai", "nana", "nanay", "nani", "nay", "nënë", "ñuke", "ñuque", "nyokap", "ôèe", "oğlum", "ojciec", "okaasan", "omm", "oppa", "otac", "otec", "otosan", "pabo", "pai", "pak", "panjo", "papa", "papá", "papà", "papi", "pappa", "pappie", "pare", "parinte", "pater", "patri", "patrino", "pedar", "pita-ji", "pitaji", "pitar", "pop", "popà", "poppa", "pops", "pradininkas", "protevis", "pupà", "reny", "salentino", "sis", "tad", "taica", "tata", "táta", "tàtah", "tatay", "tateh", "tatti", "tay", "tevas", "tevs", "teyze", "uma", "uncle", "vader", "valide", "vieja", "viejo", "yebba", "yeğen", "yenge", "badima", "memaw", "meemaw", "妈", "妈妈", "老妈", "老公", "宝贝", "老婆", "宝贝", "爸", "爸爸", "老爸", "女儿", "闺女", "儿子", "哥", "哥哥", "弟", "弟弟", "姐", "姐姐", "妹", "妹妹", "祖母", "奶奶", "大姨", "小姨", "姑姑", "舅舅", "大舅", "小舅", "叔叔", "伯伯", "表姐", "表妹", "表哥", "表弟", "侄子", "侄女", "uncle", "mama", "mamu", "chacha", "chachu", "mom", "dad", "bhai", "bhaiya", "didi" };
-
-        #endregion
-
-        public static bool MatchFromFamilyVocab(string completeName)
-        {
-            if (string.IsNullOrEmpty(completeName))
-                return false;
-
-            foreach (string vocabKey in familyVocab)
-            {
-                if (completeName.ToLower().Contains(vocabKey))
-                    return true;
-            }
-            return false;
-        }
 
     }
 }
