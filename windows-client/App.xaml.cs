8df4c0daa5fdedd1198d402b31c8b07a6bbe8f1c
rohitwp7 <rohit@bsb.in>
2013-02-27 13:30:05 +0530

    changed is_marketplace to false

--- windows-client/App.xaml.cs
+++ windows-client/App.xaml.cs
@@ -67,7 +67,7 @@ namespace windows_client
         private static string _currentVersion = "1.0.0.0";
         private static string _latestVersion;
         public static bool IS_VIEWMODEL_LOADED = false;
-        public static bool IS_MARKETPLACE = true; // change this to toggle debugging
+        public static bool IS_MARKETPLACE = false; // change this to toggle debugging
         private static bool isNewInstall = true;
         public static NewChatThread newChatThreadPage = null;
         private static bool _isTombstoneLaunch = false;
