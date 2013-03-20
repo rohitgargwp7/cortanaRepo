31ae4434256dac4b5c1fd7b8f29bf7c7e153a482
Madhur <mg@bsb.in>
2013-03-20 11:24:49 +0530

    Added a TODO for self

--- windows-client/View/NewChatThread.xaml.cs
+++ windows-client/View/NewChatThread.xaml.cs
@@ -2616,6 +2616,7 @@ namespace windows_client.View
                 object[] vals = (object[])obj;
                 ConvMessage convMessage = (ConvMessage)vals[0];
 
+                //TODO handle vibration for user profile and GC.
                 if (convMessage.Msisdn != mContactNumber || (convMessage.MetaDataString != null &&
                     convMessage.MetaDataString.Contains("poke")))
                 {
