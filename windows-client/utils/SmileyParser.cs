5388d32ce9ccd69ae03e6fd4a7c869debe9d96cf
rohitwp7 <rohit@bsb.in>
2013-05-12 01:10:29 +0530

    handled ignore case and linkify color for hyperlink

--- windows-client/utils/SmileyParser.cs
+++ windows-client/utils/SmileyParser.cs
@@ -8,6 +8,7 @@ using System.Collections.Generic;
 using System.Windows.Media.Imaging;
 using windows_client.utils;
 using Microsoft.Phone.Tasks;
+using System.Windows.Media;
 
 namespace windows_client
 {
@@ -574,7 +575,7 @@ namespace windows_client
                 patternString.Append(phoneNumberRegexPattern);
                 patternString.Append(')');
             }
-            return new Regex("(" + patternString.ToString());
+            return new Regex("(" + patternString.ToString(), RegexOptions.IgnoreCase | RegexOptions.Compiled);
         }
 
         public Paragraph LinkifyEmoticons(string messageString)
@@ -634,7 +635,7 @@ namespace windows_client
             catch { }
         }
 
-        public Paragraph LinkifyAll(string message)
+        public Paragraph LinkifyAll(string message, SolidColorBrush foreground)
         {
             MatchCollection matchCollection = ChatThreadRegex.Matches(message);
             var p = new Paragraph();
@@ -668,6 +669,7 @@ namespace windows_client
                     try
                     {
                         Hyperlink MyLink = new Hyperlink();
+                        MyLink.Foreground = foreground;
                         string url = regexMatch;
                         if (regexType == RegexType.EMAIL)
                             url = "mailto:" + regexMatch;
