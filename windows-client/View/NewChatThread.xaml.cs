08e2c36736b0853c7a71a88d3f13397a34853669
milanbsb <milan@bsb.in>
2013-06-10 18:49:26 +0530

    Emoticon palett closing on sending bug handled

--- windows-client/View/NewChatThread.xaml.cs
+++ windows-client/View/NewChatThread.xaml.cs
@@ -2738,7 +2738,9 @@ namespace windows_client.View
             sendMsgTxtbox.Text = string.Empty;
             lastText = string.Empty;
             sendIconButton.IsEnabled = false;
-            sendMsgTxtbox.Focus();
+
+            if(emoticonPanel.Visibility == Visibility.Collapsed)
+                sendMsgTxtbox.Focus();
 
             if (String.IsNullOrEmpty(message))
                 return;
@@ -2746,7 +2748,6 @@ namespace windows_client.View
             if (App.ViewModel.TipList[1].IsCurrentlyShown)
                 App.ViewModel.HideToolTip(LayoutRoot, 1);
 
-            emoticonPanel.Visibility = Visibility.Collapsed;
             attachmentMenu.Visibility = Visibility.Collapsed;
 
             if (message == "" || (!isOnHike && mCredits <= 0))
