ad68217b16df35a2f039fcf97a2ac740edf7b343
rohitgargwp7 <rohit@bsb.in>
2013-06-24 15:34:40 +0530

    changed style key name

--- windows-client/DbUtils/DbConversationListener.cs
+++ windows-client/DbUtils/DbConversationListener.cs
@@ -197,6 +197,8 @@ namespace windows_client.DbUtils
 
                     byte[] fileBytes;
                     MiscDBUtil.readFileFromIsolatedStorage(sourceFilePath, out fileBytes);
+                    if (fileBytes == null)
+                        return;
                     AccountUtils.postUploadPhotoFunction finalCallbackForUploadFile = new AccountUtils.postUploadPhotoFunction(uploadFileCallback);
                     if (!convMessage.FileAttachment.ContentType.Contains(HikeConstants.CT_CONTACT))
                         MiscDBUtil.storeFileInIsolatedStorage(HikeConstants.FILES_BYTE_LOCATION + "/" + convMessage.Msisdn + "/" +
