11888313cf956d105a8450b9f36a56d159c50307
rohit <rohit@bsb.in>
2014-01-06 16:59:31 +0530

    fixed bug:15648, No sticker downloaded in this category doesn't appears for Things Indian Say category

--- windows-client/View/NewChatThread.xaml.cs
+++ windows-client/View/NewChatThread.xaml.cs
@@ -5848,6 +5848,10 @@ namespace windows_client.View
             if (stickerCategory.ShowDownloadMessage)
             {
                 ShowDownloadOverlay(true);
+                if (stickerCategory.ListStickers.Count == 0)
+                    stickerPivot.ShowNoStickers();
+                else
+                    stickerPivot.ShowStickers();
                 return;
             }
             if (stickerCategory == null)
