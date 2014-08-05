968c81e1efde7bc756611714920ed530ce7ad5ed
RohitGarg <rohit@bsb.in>
2014-08-05 15:57:27 +0530

    fixed bug:35699, store crash on navigating from chat thread

--- windows-client/View/NewChatThread.xaml.cs
+++ windows-client/View/NewChatThread.xaml.cs
@@ -702,7 +702,7 @@ namespace windows_client.View
             if (_dt != null)
                 _dt.Stop();
 
-            if (App.ViewModel.ConvMap.ContainsKey(mContactNumber) && App.ViewModel.ConvMap[mContactNumber].DraftMessage != sendMsgTxtbox.Text.Trim())
+            if (!string.IsNullOrEmpty(mContactNumber) && App.ViewModel.ConvMap.ContainsKey(mContactNumber) && App.ViewModel.ConvMap[mContactNumber].DraftMessage != sendMsgTxtbox.Text.Trim())
             {
                 App.ViewModel.ConvMap[mContactNumber].DraftMessage = sendMsgTxtbox.Text.Trim();
                 ConversationTableUtils.saveConvObject(App.ViewModel.ConvMap[mContactNumber], mContactNumber.Replace(":", "_"));//to update file in case of tombstoning
