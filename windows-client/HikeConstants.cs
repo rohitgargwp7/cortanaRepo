5eac2844fa6a1d22ce83270376cbf0a596698246
abhi42me <abhisheka@hike.in>
2014-09-01 12:05:43 +0530

    UI fixes

--- windows-client/HikeConstants.cs
+++ windows-client/HikeConstants.cs
@@ -108,6 +108,7 @@ namespace windows_client
         public static readonly int TYPING_NOTIFICATION_AUTOHIDE = 6; //seconds
         public static readonly int SEND_START_TYPING_TIMER = 4; //seconds
         public static readonly int MAX_CHATBUBBLE_SIZE = 1400;//chars
+        public static readonly int STARTING_BASE_YEAR = 1600; //file time is ticks starting from jan 1 1601 so adding 1600 years
 
         public static readonly int ANALYTICS_POST_TIME = 12;//hours on prod and minutes on staging
         public static readonly string ANALYTICS_OBJECT_FILE = "eventsFile";
