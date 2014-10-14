3f9c6ebe1cc8fe93ac5910950ecedfd99773619e
GG-hike <gagan@hike.in>
2014-10-14 13:00:48 +0530

    Bug 38050 resolved

--- windows-client/utils/TimeUtils.cs
+++ windows-client/utils/TimeUtils.cs
@@ -2,6 +2,7 @@
 using System.Text;
 using windows_client.Languages;
 using System.Globalization;
+using System.Diagnostics;
 
 namespace windows_client.utils
 {
@@ -233,10 +234,19 @@ namespace windows_client.utils
 
         public static string GetOnHikeSinceDisplay(long timestamp)
         {
-            long ticks = timestamp * 10000000;
-            ticks += DateTime.Parse("01/01/1970 00:00:00").Ticks;
-            DateTime messageTime = new DateTime(ticks);
-            return messageTime.ToString("MMM yyyy", CultureInfo.CurrentUICulture);
+            try
+            {
+                long ticks = timestamp * 10000000;
+                ticks += DateTime.Parse("01/01/1970 00:00:00").Ticks;
+                DateTime messageTime = new DateTime(ticks);
+                return messageTime.ToString("MMM yyyy", CultureInfo.CurrentUICulture);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Error while GetOnHikeSinceDisplay() " + ex.StackTrace);
+            }
+
+            return String.Empty;
         }
 
     }
