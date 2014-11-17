aac2c3a0433066ac11f837aee7c939ce1b95d8a1
mohitminto <mohit@hike.in>
2014-11-17 19:51:42 +0530

    adding new files for sml

---
+++ windows-client/View/SetImageQuality.xaml.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using System.Windows.Media;
+using windows_client.Languages;
+using windows_client.utils;
+
+namespace windows_client.View
+{
+    public partial class SetImageQuality : PhoneApplicationPage
+    {
+        ApplicationBarIconButton picturesUpload;
+
+        public SetImageQuality()
+        {
+            InitializeComponent();
+            InitialiseAppBar();
+        }
+
+        private void InitialiseAppBar()
+        {
+            ApplicationBar = new ApplicationBar()
+            {
+                ForegroundColor = (Color)App.Current.Resources["AppBarBlackForegroundColor"],
+                BackgroundColor = (Color)App.Current.Resources["AppBarBlackBackgroundColor"],
+            };
+
+            picturesUpload = new ApplicationBarIconButton();
+            picturesUpload.IconUri = new Uri("/View/images/AppBar/icon_send.png", UriKind.RelativeOrAbsolute);
+            picturesUpload.Text = AppResources.Send_Txt;
+            picturesUpload.Click += OnPicturesUploadClick;
+            ApplicationBar.Buttons.Add(picturesUpload);
+        }
+
+        private void OnPicturesUploadClick(object sender, EventArgs e)
+        {
+            //PhoneApplicationService.Current.State[HikeConstants.NavigationKeys.MULTIPLE_IMAGES] = ImageQualityBox.SelectedIndex;
+            PhoneApplicationService.Current.State[HikeConstants.IMAGE_QUALITY] = ImageQualityBox.SelectedIndex;
+
+            //previous page is viewalbums page and then new chat thread page
+            if (NavigationService.CanGoBack)
+                NavigationService.RemoveBackEntry();
+            if (NavigationService.CanGoBack)
+                NavigationService.RemoveBackEntry();
+            if (NavigationService.CanGoBack)
+                NavigationService.GoBack();
+
+            //clear thumbnail cache as it is not required now
+            App.ViewModel.ClearMFtImageCache();
+        }
+        /*
+        private void ImageQualityBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            
+            if (ImageQualityBox != null)
+            {
+                try
+                {
+                    ListBox listBox = sender as ListBox;
+                    ResetImageQualityBox();
+                    if (listBox.SelectedIndex == 0)
+                    {
+                        normalBox.Foreground = UI_Utils.Instance.HikeBlue;
+                    }
+                    else if (listBox.SelectedIndex == 1)
+                    {
+                        betterBox.Foreground = UI_Utils.Instance.HikeBlue;
+                    }
+                    else
+                    {
+                        uncompressedBox.Foreground = UI_Utils.Instance.HikeBlue;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+            }
+             
+        }* */
+        #region Page Functions
+
+        protected override void OnBackKeyPress(System.ComponentModel.CancelEventArgs e)
+        {
+            base.OnBackKeyPress(e);
+        }
+
+        #endregion
+    }
+}
